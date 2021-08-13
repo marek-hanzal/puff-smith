4d1771dd6dce372119b38ca0851b8b41345fbfaf
Marek Hanzal <marek.hanzal@x32.cz>
2021-08-13 09:30:33 +0200

    Common: Prepared very first client project

---
+++ client/README.md
@@ -0,0 +1,3 @@
+## Client Application
+
+This is a Vapers Dream default (reference) client application.
---
+++ client/next.config.js
@@ -0,0 +1,25 @@
+module.exports = require("next-compose-plugins")([
+	require("next-images"),
+	require("@next/bundle-analyzer")({
+		enabled: process.env.ANALYZE === "true",
+	}),
+	[require("next-plugin-antd-less"), {
+		modifyVars: {
+			// "@primary-color": "#002c76",
+			// "@link-color": "#002c76",
+		},
+	}],
+], {
+	reactStrictMode: true,
+	basePath: process.env.NEXT_PUBLIC_PUBLIC_URL,
+	async rewrites() {
+		return [
+			{
+				source: "/blackfox/:path*",
+				destination: process.env.NEXT_PUBLIC_BLACKFOX + "/blackfox/:path*",
+				basePath: false,
+				locale: false,
+			},
+		];
+	}
+});
