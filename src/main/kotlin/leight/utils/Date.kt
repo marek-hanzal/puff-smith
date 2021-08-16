cd8928e4d65bcc4b0897b369bd79dc378b322198
Marek Hanzal <marek.hanzal@x32.cz>
2021-08-16 20:31:43 +0200

    Common: Some minor changes II

--- src/main/kotlin/leight/utils/Date.kt
+++ src/main/kotlin/leight/utils/Date.kt
@@ -3,8 +3,8 @@ package leight.utils
 import org.joda.time.DateTime
 import org.joda.time.format.ISODateTimeFormat
 
-fun DateTime.asDate() = this.toString("YYYY-MM-dd")
+fun DateTime.asDate(): String = this.toString("YYYY-MM-dd")
 
-fun DateTime.asStamp() = this.toString("YYYY-MM-dd HH:mm:ss.SSS")
+fun DateTime.asStamp(): String = this.toString("YYYY-MM-dd HH:mm:ss.SSS")
 
-fun DateTime.asIso() = this.toLocalDateTime().toString(ISODateTimeFormat.dateTime())
+fun DateTime.asIso(): String = this.toLocalDateTime().toString(ISODateTimeFormat.dateTime())
