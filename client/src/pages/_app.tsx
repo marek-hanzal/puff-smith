7a81e050731680721bcb5e45c16aa4acdb1ba64a
Marek Hanzal <marek.hanzal@x32.cz>
2021-08-17 20:29:27 +0200

    Common: Fixed build

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import {bootstrap, LogoIcon} from "@/vapers-dream";
+import {bootstrap} from "@/vapers-dream";
 import "@/vapers-dream/assets/styles/globals.css";
 import {App, IPageWithLayout} from "@leight-core/leight";
 import type {AppProps} from "next/app";
@@ -6,9 +6,7 @@ import type {AppProps} from "next/app";
 // noinspection JSUnusedGlobalSymbols
 export default function VapersDream({Component, pageProps}: AppProps) {
 	bootstrap();
-	return <App
-		icon={<LogoIcon style={{width: "10vw"}}/>}
-	>
+	return <App>
 		{((Component as IPageWithLayout<any>).layout || (page => page))(<Component {...pageProps}/>)}
 	</App>;
 }
