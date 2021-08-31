eaaf41b73f6752ef930a6a12b7a71d2879270b9c
Marek Hanzal <marek.hanzal@x32.cz>
2021-08-31 18:09:42 +0200

    Common: An attempt to optimize docker build stuff

--- client/src/pages/root/index.tsx
+++ client/src/pages/root/index.tsx
@@ -1,6 +1,11 @@
+import {LogoFullIcon} from "@/ps";
 import {RootPage, withRootLayout} from "@/ps/site/root";
+import {Result} from "antd";
 
 export default withRootLayout(function Index() {
 	return <RootPage name={"root.index"}>
+		<Result
+			icon={<LogoFullIcon/>}
+		/>
 	</RootPage>;
 });
