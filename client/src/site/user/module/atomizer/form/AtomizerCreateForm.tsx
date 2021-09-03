14061cba1e3c754d69be7c2576cec12a2d71f6e9
Marek Hanzal <marek.hanzal@x32.cz>
2021-09-03 08:18:36 +0200

    Common: Prepared atomizer form... looks quite good; now it's time for some kotlinning

--- client/src/site/user/module/atomizer/form/AtomizerCreateForm.tsx
+++ client/src/site/user/module/atomizer/form/AtomizerCreateForm.tsx
@@ -2,7 +2,7 @@ import {AtomizerIcon, ps} from "@/ps";
 import {CoilsBaseSelect, CoilSizeSelect} from "@/ps/site/user/module/atomizer";
 import {VendorSelect} from "@/ps/site/user/module/vendor";
 import {Centered, Form, FormItem, IFormProps, Submit, SwitchItem} from "@leight-core/leight";
-import {InputNumber} from "antd";
+import {Divider, InputNumber} from "antd";
 import {FC} from "react";
 
 const doCreate = ps.user.atomizer.doCreate;
@@ -17,13 +17,20 @@ export const AtomizerCreateForm: FC<IAtomizerCreateFormProps> = props => {
 		post={doCreate}
 		{...props}
 	>
-		<FormItem field={"name"} labels={["user.atomizer.name.label"]} required tooltip={"user.atomizer.name.label.tooltip"}/>
-		<FormItem field={"code"} labels={["user.atomizer.code.label"]} required tooltip={"user.atomizer.code.label.tooltip"}/>
 		<FormItem field={"vendorId"} labels={["user.atomizer.vendor.label"]} required tooltip={"user.atomizer.vendor.label.tooltip"}>
 			<VendorSelect category={"atomizer"}/>
 		</FormItem>
-		<FormItem field={"coils"} labels={["user.atomizer.coils.label"]} tooltip={"user.atomizer.coils.label.tooltip"}>
-			<CoilsBaseSelect/>
+		<Divider/>
+		<FormItem field={"name"} labels={["user.atomizer.name.label"]} required tooltip={"user.atomizer.name.label.tooltip"}/>
+		<FormItem field={"code"} labels={["user.atomizer.code.label"]} required tooltip={"user.atomizer.code.label.tooltip"}/>
+		<Divider/>
+		<FormItem field={"base"} labels={["user.atomizer.base.label"]} tooltip={"user.atomizer.base.label.tooltip"}>
+			<InputNumber
+				step={0.5}
+				min={0}
+				max={38}
+				style={{width: "100%"}}
+			/>
 		</FormItem>
 		<FormItem field={"capacity"} labels={["user.atomizer.capacity.label"]} tooltip={"user.atomizer.capacity.label.tooltip"}>
 			<InputNumber
@@ -32,6 +39,11 @@ export const AtomizerCreateForm: FC<IAtomizerCreateFormProps> = props => {
 				max={14}
 				style={{width: "100%"}}
 			/>
+		</FormItem>
+		<SwitchItem field={"squonk"} labels={["user.atomizer.squonk.label"]} tooltip={"user.atomizer.squonk.label.tooltip"}/>
+		<Divider/>
+		<FormItem field={"coils"} labels={["user.atomizer.coils.label"]} tooltip={"user.atomizer.coils.label.tooltip"}>
+			<CoilsBaseSelect/>
 		</FormItem>
 		<FormItem field={"maxCoilSize"} labels={["user.atomizer.max-coil-size.label"]} tooltip={"user.atomizer.max-coil-size.label.tooltip"}>
 			<CoilSizeSelect/>
@@ -44,7 +56,7 @@ export const AtomizerCreateForm: FC<IAtomizerCreateFormProps> = props => {
 				style={{width: "100%"}}
 			/>
 		</FormItem>
-		<SwitchItem field={"squonk"} labels={["user.atomizer.squonk.label"]} tooltip={"user.atomizer.squonk.label.tooltip"}/>
+		<Divider/>
 		<Centered>
 			<Submit icon={<AtomizerIcon/>} size={"large"} label={"user.atomizer.create.label"}/>
 		</Centered>
