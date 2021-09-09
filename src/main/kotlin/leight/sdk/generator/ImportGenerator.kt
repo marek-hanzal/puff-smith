0876ff0898bdc1584a5448e5b14f0ae36b6874c4
Marek Hanzal <marek.hanzal@x32.cz>
2021-09-09 21:16:45 +0200

    Common: Import generator looks promising

--- src/main/kotlin/leight/sdk/generator/ImportGenerator.kt
+++ src/main/kotlin/leight/sdk/generator/ImportGenerator.kt
@@ -4,31 +4,37 @@ import leight.client.sdk.annotation.TypeArrayClass
 import leight.client.sdk.annotation.TypeClass
 import leight.container.AbstractService
 import leight.container.IContainer
+import leight.rest.IEndpoint
 import leight.sdk.annotation.Module
-import leight.sdk.utils.ClassContext
+import leight.sdk.lazyClassExtractor
 import kotlin.reflect.KClass
 import kotlin.reflect.full.findAnnotation
 import kotlin.reflect.full.memberProperties
 
 class ImportGenerator(container: IContainer) : AbstractService(container) {
+	private val classExtractor by container.lazyClassExtractor()
+
 	private fun generateImport(klass: KClass<*>, module: Module): String {
 		return "import {${klass.simpleName!!}} from \"@/sdk/${module.name}\""
 	}
 
-	private fun forClass(klass: KClass<*>) = sequence {
-		klass.findAnnotation<Module>()?.let { module ->
+	private fun forClass(klass: KClass<*>, module: Module) = sequence {
+		klass.findAnnotation<Module>()?.let { klassModule ->
+			if (module.name != klassModule.name) {
+				yield(module.name to generateImport(klass, klassModule))
+			}
 			klass.memberProperties.forEach { kProperty ->
 				kProperty.findAnnotation<TypeClass>()?.let { typeClass ->
 					typeClass.klass.findAnnotation<Module>()?.let { innerModule ->
-						if (innerModule.name != module.name) {
-							yield(module.name to generateImport(typeClass.klass, innerModule))
+						if (innerModule.name != klassModule.name) {
+							yield(klassModule.name to generateImport(typeClass.klass, innerModule))
 						}
 					}
 				}
 				kProperty.findAnnotation<TypeArrayClass>()?.let { typeArrayClass ->
 					typeArrayClass.target.klass.findAnnotation<Module>()?.let { innerModule ->
-						if (innerModule.name != module.name) {
-							yield(module.name to generateImport(typeArrayClass.target.klass, innerModule))
+						if (innerModule.name != klassModule.name) {
+							yield(klassModule.name to generateImport(typeArrayClass.target.klass, innerModule))
 						}
 					}
 				}
@@ -36,20 +42,26 @@ class ImportGenerator(container: IContainer) : AbstractService(container) {
 		}
 	}
 
-	private fun forTypeClass(typeClass: TypeClass) = sequence {
-		yieldAll(forClass(typeClass.klass))
+	private fun forTypeClass(typeClass: TypeClass, module: Module) = sequence {
+		yieldAll(forClass(typeClass.klass, module))
 		typeClass.types.forEach {
-			yieldAll(forClass(it.klass))
+			yieldAll(forClass(it.klass, module))
 		}
 	}
 
-	fun generate(classList: Sequence<ClassContext>) = sequence {
+	fun generate(endpoints: List<KClass<out IEndpoint>>) = sequence {
 		val dependencies = mutableMapOf<String, MutableList<String>>()
 
-		classList.forEach { classContext ->
-			forClass(classContext.klazz).forEach { (module, source) -> dependencies.getOrPut(module) { mutableListOf() }.add(source) }
-			forTypeClass(classContext.endpoint.request).forEach { (module, source) -> dependencies.getOrPut(module) { mutableListOf() }.add(source) }
-			forTypeClass(classContext.endpoint.response).forEach { (module, source) -> dependencies.getOrPut(module) { mutableListOf() }.add(source) }
+		classExtractor.toClassList(endpoints).forEach { classContext ->
+			forTypeClass(classContext.typeClass, classContext.module).forEach { (module, source) -> dependencies.getOrPut(module) { mutableListOf() }.add(source) }
+		}
+		classExtractor.toExport(endpoints).forEach { exportContext ->
+			forClass(exportContext.endpoint.request.klass, exportContext.module).forEach { (module, source) ->
+				dependencies.getOrPut(module) { mutableListOf() }.add(source)
+			}
+			forClass(exportContext.endpoint.response.klass, exportContext.module).forEach { (module, source) ->
+				dependencies.getOrPut(module) { mutableListOf() }.add(source)
+			}
 		}
 
 		dependencies.forEach { (module, source) ->
