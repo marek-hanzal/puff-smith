535e30a7dc129c40e59091164477ed8596b0c49b
Marek Hanzal <marek.hanzal@x32.cz>
2021-09-14 10:03:16 +0200

    Common: A little bit improved generator code

--- src/main/kotlin/leight/sdk/generator/EndpointGenerator.kt
+++ src/main/kotlin/leight/sdk/generator/EndpointGenerator.kt
@@ -8,38 +8,31 @@ import leight.sdk.lazyNameResolver
 import leight.sdk.utils.ExportContext
 
 class EndpointGenerator(container: IContainer) : AbstractService(container) {
-	private val genericGenerator by container.lazyGenericGenerator()
 	private val endpointInfo by container.lazyEndpointInfo()
 	private val nameResolver by container.lazyNameResolver()
+	private val typeGenerator by container.lazyTypeGenerator()
 
 	fun generate(exportContext: ExportContext): String = nameResolver.simpleName(exportContext.klazz).let { name ->
+		val request = typeGenerator.resolve(exportContext.endpoint.request)
+		val response = typeGenerator.resolve(exportContext.endpoint.response)
+		val id = endpointInfo.getId(exportContext.klazz)
+		val pair = "$request, $response"
+
 		when (exportContext.endpoint.method) {
 			EndpointMethod.GET -> {
-				"export const do${name}Fetch = createGet<${genericGenerator.exportExpandedClass(exportContext.endpoint.response)}>(\"${endpointInfo.getId(exportContext.klazz)}\")"
+				"export const do${name} = createGet<$response>(\"$id\")"
 			}
 			EndpointMethod.POST -> {
-				"export const do${name} = createPost<${genericGenerator.exportExpandedClass(exportContext.endpoint.request)}, ${genericGenerator.exportExpandedClass(exportContext.endpoint.response)}>(\"${
-					endpointInfo.getId(
-						exportContext.klazz
-					)
-				}\")"
+				"export const do${name} = createPost<$pair>(\"$id\")"
 			}
 			EndpointMethod.PATCH -> {
-				"export const do${name} = createPatch<${genericGenerator.exportExpandedClass(exportContext.endpoint.request)}, ${genericGenerator.exportExpandedClass(exportContext.endpoint.response)}>(\"${
-					endpointInfo.getId(
-						exportContext.klazz
-					)
-				}\")"
+				"export const do${name} = createPatch<$pair>(\"$id\")"
 			}
 			EndpointMethod.PUT -> {
-				"export const do${name} = createPut<${genericGenerator.exportExpandedClass(exportContext.endpoint.request)}, ${genericGenerator.exportExpandedClass(exportContext.endpoint.response)}>(\"${
-					endpointInfo.getId(
-						exportContext.klazz
-					)
-				}\")"
+				"export const do${name} = createPut<$pair>(\"$id\")"
 			}
 			EndpointMethod.DELETE -> {
-				"export const do${name} = createDelete<${genericGenerator.exportExpandedClass(exportContext.endpoint.response)}>(\"${endpointInfo.getId(exportContext.klazz)}\")"
+				"export const do${name} = createDelete<$response>(\"$id\")"
 			}
 		}.let { "$it;" }
 	}
