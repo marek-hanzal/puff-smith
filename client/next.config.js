64cce351c74b995c7f951002c5dffccf8ffa275c
Marek Hanzal <marek.hanzal@x32.cz>
2021-11-25 10:02:41 +0100

    Common: Just some minor things

--- client/next.config.js
+++ client/next.config.js
@@ -5,13 +5,18 @@ module.exports = require("next-compose-plugins")([
 	}),
 	require("next-plugin-antd-less"),
 ], {
+	swcMinify: true,
+	images: {
+		formats: ["image/avif", "image/webp"]
+	},
 	reactStrictMode: true,
+	staticPageGenerationTimeout: 15,
 	basePath: process.env.NEXT_PUBLIC_PUBLIC_URL,
 	async rewrites() {
 		return [
 			{
 				source: "/api/:path*",
-				destination: "http://localhost:8088/api/:path*",
+				destination: process.env.NEXT_PUBLIC_BACKEND + "/api/:path*",
 				basePath: false,
 				locale: false,
 			},
