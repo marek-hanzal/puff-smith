da3c674428c4df092f877549fdbe90f1a38cfae4
Marek Hanzal <marek.hanzal@x32.cz>
2022-01-22 19:38:55 +0100

    Common: Some new menu icons

--- client/src/site/lab/@module/menu/LabMenu.tsx
+++ client/src/site/lab/@module/menu/LabMenu.tsx
@@ -1,6 +1,6 @@
 import {CreateMenuItem, HomeIcon, MenuDivider} from "@leight-core/leight";
 import {FC} from "react";
-import {AtomizerIcon, BuildIcon, CoilIcon, CottonIcon, IMenuProps, LiquidIcon, Menu, VapeIcon, VendorIcon, WireIcon} from "@/puff-smith";
+import {AtomizerIcon, BuildIcon, CoilIcon, CottonIcon, IMenuProps, LiquidIcon, Menu, SteepIcon, ToolIcon, VapeIcon, VendorIcon, WireIcon} from "@/puff-smith";
 import {ModIcon} from "@/puff-smith/component/icon/ModIcon";
 import {CellIcon} from "@/puff-smith/component/icon/CellIcon";
 
@@ -16,6 +16,7 @@ export const LabMenu: FC<ILabMenuProps> = props => {
 		{CreateMenuItem("lab.vape.menu", "/lab/vape", <VapeIcon/>)}
 		<MenuDivider/>
 		{CreateMenuItem("lab.liquid.menu", "/lab/liquid", <LiquidIcon/>)}
+		{CreateMenuItem("lab.steep.menu", "/lab/steep", <SteepIcon/>)}
 		<MenuDivider/>
 		{CreateMenuItem("lab.atomizer.menu", "/lab/atomizer", <AtomizerIcon/>)}
 		{CreateMenuItem("lab.mod.menu", "/lab/mod", <ModIcon/>)}
@@ -25,5 +26,7 @@ export const LabMenu: FC<ILabMenuProps> = props => {
 		{CreateMenuItem("lab.cotton.menu", "/lab/cotton", <CottonIcon/>)}
 		{CreateMenuItem("lab.wire.menu", "/lab/wire", <WireIcon/>)}
 		{CreateMenuItem("lab.coil.menu", "/lab/coil", <CoilIcon/>)}
+		<MenuDivider/>
+		{CreateMenuItem("lab.tool.menu", "/lab/tool", <ToolIcon/>)}
 	</Menu>;
 };
