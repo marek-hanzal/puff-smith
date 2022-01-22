3af26ef8ab2339a852d70307c098b48a9339bfdb
Marek Hanzal <marek.hanzal@x32.cz>
2022-01-22 02:24:20 +0100

    Common: Fixed some missing docs

--- src/PuffSmith/Cotton/Dto/CottonDto.php
+++ src/PuffSmith/Cotton/Dto/CottonDto.php
@@ -7,9 +7,14 @@ use Edde\Dto\AbstractDto;
 use PuffSmith\Vendor\Dto\VendorDto;
 
 class CottonDto extends AbstractDto {
+	/** @var string */
 	public string $id;
+	/** @var string */
 	public string $name;
+	/** @var string|null */
 	public ?string $description;
+	/** @var string */
 	public string $vendorId;
+	/** @var VendorDto */
 	public VendorDto $vendor;
 }
--- src/PuffSmith/Cotton/Dto/Create/CreateDto.php
+++ src/PuffSmith/Cotton/Dto/Create/CreateDto.php
@@ -6,7 +6,10 @@ namespace PuffSmith\Cotton\Dto\Create;
 use Edde\Dto\AbstractDto;
 
 class CreateDto extends AbstractDto {
+	/** @var string */
 	public string $name;
+	/** @var string|null */
 	public ?string $description;
+	/** @var string */
 	public string $vendorId;
 }
