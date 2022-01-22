9e63f2742b3ca30708161ecfc3e03f141cf99cbe
Marek Hanzal <marek.hanzal@x32.cz>
2022-01-22 02:16:17 +0100

    Common: Fixed build

--- client/src/site/lab/coil/@module/form/CoilSelect.tsx
+++ client/src/site/lab/coil/@module/form/CoilSelect.tsx
@@ -14,7 +14,7 @@ export const CoilSelect: FC<ICoilSelectProps> = props => {
 				{coil.code} - <Typography.Text>{coil.wire.name}</Typography.Text>&nbsp;
 				<Typography.Text type={'secondary'}>{coil.wire.vendor.name}</Typography.Text><br/>
 				<Typography.Text type={'secondary'}>{(coil.wire.ga ? coil.wire.ga + 'GA' : null) || coil.wire.description}</Typography.Text><br/>
-				<Typography.Text>{parseFloat(coil.ohm).toFixed(2)}ohm</Typography.Text>
+				<Typography.Text>{coil.ohm.toFixed(2)}ohm</Typography.Text>
 			</>,
 			value: coil.id,
 			...coil,
