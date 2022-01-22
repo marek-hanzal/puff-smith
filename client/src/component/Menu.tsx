0224f7d81512df61b02ccfa68b80c8c1d22b6a97
Marek Hanzal <marek.hanzal@x32.cz>
2022-01-22 16:00:25 +0100

    Common: Trying to fix menu shit

--- client/src/component/Menu.tsx
+++ client/src/component/Menu.tsx
@@ -1,11 +1,12 @@
-import {IMenuProps as ICoolMenuProps, Menu as CoolMenu, MenuPortal} from "@leight-core/leight";
+import {IMenuProps as ICoolMenuProps, Menu as CoolMenu, MenuPortal, useMenuContext} from "@leight-core/leight";
 import {FC} from "react";
 
 export interface IMenuProps extends Partial<ICoolMenuProps> {
 }
 
 export const Menu: FC<IMenuProps> = props => {
+	const menuContext = useMenuContext();
 	return <MenuPortal>
-		<CoolMenu {...props}/>
+		<CoolMenu inlineCollapsed={menuContext.collapsed} {...props}/>
 	</MenuPortal>;
 };
