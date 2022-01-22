4a4c1ce105bd31170f5247016f62b4d3f1b2231e
Marek Hanzal <marek.hanzal@x32.cz>
2022-01-22 20:56:06 +0100

    Common: Enabled cache

--- client/src/site/shared/@module/layout/@module/AppLayout.tsx
+++ client/src/site/shared/@module/layout/@module/AppLayout.tsx
@@ -3,6 +3,7 @@ import dayjs from "dayjs";
 import i18next from "i18next";
 import {FC} from "react";
 import {FullLogoIcon} from "@/puff-smith";
+import {useQueryPersistence} from "@leight-core/leight/dist";
 
 const queryClient = createQueryClient();
 
@@ -10,7 +11,7 @@ export interface IAppLayoutProps extends Partial<IAppProps> {
 }
 
 export const AppLayout: FC<IAppLayoutProps> = props => {
-	// useQueryPersistence(queryClient, "puff-smith");
+	useQueryPersistence(queryClient, "puff-smith");
 
 	return <I18NextProvider i18next={i18next}>
 		<DayjsContextProvider dayjs={dayjs}>
