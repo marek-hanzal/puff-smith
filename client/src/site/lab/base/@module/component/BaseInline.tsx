0f792a732b8648be4779ad0f3705ac3b33e967bf
Marek Hanzal <marek.hanzal@x32.cz>
2022-01-23 03:46:57 +0100

    Common: Prepared a lot of interesting stuff

--- client/src/site/lab/base/@module/component/BaseInline.tsx
+++ client/src/site/lab/base/@module/component/BaseInline.tsx
@@ -3,12 +3,12 @@ import {FC} from "react";
 import {Typography} from "antd";
 
 export interface IBaseInlineProps {
-	base: BaseDto
+	base: BaseDto | null;
 }
 
 export const BaseInline: FC<IBaseInlineProps> = ({base}) => {
-	return <>
+	return base ? <>
 		{base.name}&nbsp;<Typography.Text type={'secondary'}>{base.vendor.name}</Typography.Text><br/>
 		{base.pg}/{base.vg} PG/VG
-	</>
+	</> : <>-</>
 }
--- client/src/site/lab/mixture/@module/table/MixtureTable.tsx
+++ client/src/site/lab/mixture/@module/table/MixtureTable.tsx
@@ -1,8 +1,86 @@
 import {FC} from "react";
+import {IMixturesSourceTableProps, MixturesSourceTable} from "@/sdk/puff-smith/api/lab/mixture/endpoint";
+import {toLocalDate} from "@leight-core/leight";
+import {LiquidInline} from "@/puff-smith/site/lab/liquid";
+import {BoosterInline} from "@/puff-smith/site/lab/booster";
+import {BaseInline} from "@/puff-smith/site/lab/base";
 
-export interface IMixtureTableProps {
+export interface IMixtureTableProps extends Partial<IMixturesSourceTableProps> {
 }
 
 export const MixtureTable: FC<IMixtureTableProps> = props => {
-	return <>nope</>;
+	return <MixturesSourceTable
+		scroll={{x: 2000}}
+		{...props}
+	>
+		{({column}) => [
+			column({
+				key: "name",
+				dataIndex: "name",
+				title: "lab.mixture.table.name",
+				width: 200,
+			}),
+			column({
+				key: "code",
+				dataIndex: "code",
+				title: "lab.mixture.table.code",
+				width: 160,
+			}),
+			column({
+				key: "liquid",
+				title: "lab.mixture.table.liquid",
+				width: 240,
+				render: (_, mixture) => <LiquidInline liquid={mixture.liquid}/>,
+			}),
+			column({
+				key: "base",
+				title: "lab.mixture.table.base",
+				width: 200,
+				render: (_, mixture) => <BaseInline base={mixture.base}/>,
+			}),
+			column({
+				key: "booster",
+				title: "lab.mixture.table.booster",
+				width: 200,
+				render: (_, mixture) => <BoosterInline booster={mixture.booster}/>,
+			}),
+			column({
+				key: "pgvg",
+				title: "lab.mixture.table.pgvg",
+				width: 100,
+				render: (_, mixture) => <>{mixture.pg}/{mixture.vg}</>
+			}),
+			column({
+				key: "nicotine",
+				dataIndex: "nicotine",
+				title: "lab.mixture.table.nicotine",
+				width: 160,
+				render: nicotine => <>{nicotine}mg</>
+			}),
+			column({
+				key: "mixed",
+				title: "lab.mixture.table.mixed",
+				width: 160,
+				render: (_, mixture) => toLocalDate(mixture.mixed),
+			}),
+			column({
+				key: "expires",
+				title: "lab.mixture.table.expires",
+				width: 160,
+				render: (_, mixture) => toLocalDate(mixture.expires),
+			}),
+			column({
+				key: "volume",
+				dataIndex: "volume",
+				title: "lab.mixture.table.volume",
+				width: 120,
+				render: volume => <>{volume}ml</>
+			}),
+			column({
+				key: "steep",
+				dataIndex: "steep",
+				title: "lab.mixture.table.steep",
+			}),
+		]}
+	</MixturesSourceTable>;
 }
