e6c04f5e746022a24d542efa4beddbdd89a7eceb
Marek Hanzal <marek.hanzal@x32.cz>
2022-01-23 08:02:09 +0100

    Common: A bit of boring stuff

--- src/PuffSmith/Vape/Dto/Create/CreateDto.php
+++ src/PuffSmith/Vape/Dto/Create/CreateDto.php
@@ -6,4 +6,72 @@ namespace PuffSmith\Vape\Dto\Create;
 use Edde\Dto\AbstractDto;
 
 class CreateDto extends AbstractDto {
+	/**
+	 * @var string
+	 */
+	public string $setupId;
+	/**
+	 * @var string
+	 */
+	public string $mixtureId;
+	/**
+	 * @var string|null
+	 */
+	public ?string $driptipId;
+	/**
+	 * @var int
+	 */
+	public int $rating;
+	/**
+	 * @var int
+	 */
+	public int $taste;
+	/**
+	 * @var int|null
+	 */
+	public ?int $fruits;
+	/**
+	 * @var int|null
+	 */
+	public ?int $tobacco;
+	/**
+	 * @var int|null
+	 */
+	public ?int $cakes;
+	/**
+	 * @var int|null
+	 */
+	public ?int $complex;
+	/**
+	 * @var int|null
+	 */
+	public ?int $fresh;
+	/**
+	 * @var int
+	 */
+	public int $clouds;
+	/**
+	 * @var int
+	 */
+	public int $mtl;
+	/**
+	 * @var int
+	 */
+	public int $dl;
+	/**
+	 * @var int
+	 */
+	public int $airflow;
+	/**
+	 * @var int|null
+	 */
+	public ?int $juice;
+	/**
+	 * @var int|null
+	 */
+	public ?int $power;
+	/**
+	 * @var int|null
+	 */
+	public ?int $tc;
 }
--- src/PuffSmith/Vape/Dto/VapeDto.php
+++ src/PuffSmith/Vape/Dto/VapeDto.php
@@ -4,10 +4,97 @@ declare(strict_types=1);
 namespace PuffSmith\Vape\Dto;
 
 use Edde\Dto\AbstractDto;
+use PuffSmith\Driptip\Dto\DriptipDto;
+use PuffSmith\Mixture\Dto\MixtureDto;
+use PuffSmith\Setup\Dto\SetupDto;
 
 class VapeDto extends AbstractDto {
 	/**
 	 * @var string
 	 */
 	public string $id;
+	/**
+	 * @var string
+	 */
+	public string $setupId;
+	/**
+	 * @var SetupDto
+	 */
+	public SetupDto $setup;
+	/**
+	 * @var string
+	 */
+	public string $mixtureId;
+	/**
+	 * @var MixtureDto
+	 */
+	public MixtureDto $mixture;
+	/**
+	 * @var string|null
+	 */
+	public ?string $driptipId;
+	/**
+	 * @var DriptipDto|null
+	 */
+	public ?DriptipDto $driptip;
+	/**
+	 * @var int
+	 */
+	public int $rating;
+	/**
+	 * @var int
+	 */
+	public int $taste;
+	/**
+	 * @var int|null
+	 */
+	public ?int $fruits;
+	/**
+	 * @var int|null
+	 */
+	public ?int $tobacco;
+	/**
+	 * @var int|null
+	 */
+	public ?int $cakes;
+	/**
+	 * @var int|null
+	 */
+	public ?int $complex;
+	/**
+	 * @var int|null
+	 */
+	public ?int $fresh;
+	/**
+	 * @var int
+	 */
+	public int $clouds;
+	/**
+	 * @var int
+	 */
+	public int $mtl;
+	/**
+	 * @var int
+	 */
+	public int $dl;
+	/**
+	 * @var int
+	 */
+	public int $airflow;
+	/**
+	 * @var int|null
+	 */
+	public ?int $juice;
+	/**
+	 * @var int|null
+	 */
+	public ?int $power;
+	/**
+	 * @var int|null
+	 */
+	public ?int $tc;
+	/**
+	 * @var string
+	 */
+	public string $stamp;
 }
