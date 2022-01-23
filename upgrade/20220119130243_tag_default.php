a9de99ed94cf892d83b14206301f639751a6a92f
Marek Hanzal <marek.hanzal@x32.cz>
2022-01-23 04:37:49 +0100

    Common: Added tag import support

--- upgrade/20220119130243_tag_default.php
+++ upgrade/20220119130243_tag_default.php
@@ -4,4 +4,8 @@ declare(strict_types=1);
 use Edde\Upgrade\TagUpgrade;
 
 final class TagDefault extends TagUpgrade {
+	public function change(): void {
+		parent::change();
+		$this->importExcel(__DIR__ . '/fixtures/tags.xlsx');
+	}
 }
