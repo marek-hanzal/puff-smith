729d0492cdc450ca8dcab7307b84da030de02474
Marek Hanzal <marek.hanzal@x32.cz>
2022-01-31 17:50:49 +0100

    Common: Coil stuff done

--- client/src/site/lab/coil/@module/component/CoilQuickMenu.tsx
+++ client/src/site/lab/coil/@module/component/CoilQuickMenu.tsx
@@ -1,14 +1,19 @@
 import {IQuickMenuProps, QuickMenu} from "@leight-core/leight/dist";
 import {FC} from "react";
 import {CoilDto} from "@/sdk/puff-smith/coil/dto";
-import {Menu} from "antd";
-import {CoilCloneButton, CoilEditButton, CoilLinkButton, CoilPreviewButton} from "@/puff-smith/site/lab/coil";
+import {Menu, message} from "antd";
+import {CoilCloneButton, CoilDeleteButton, CoilEditButton, CoilLinkButton, CoilPreviewButton} from "@/puff-smith/site/lab/coil";
+import {useCoilsQueryInvalidate, useDeleteMutation} from "@/sdk/puff-smith/api/lab/coil/endpoint";
+import {useTranslation} from "react-i18next";
 
 export interface ICoilQuickMenuProps extends Partial<IQuickMenuProps> {
 	coil: CoilDto;
 }
 
 export const CoilQuickMenu: FC<ICoilQuickMenuProps> = ({coil, ...props}) => {
+	const {t} = useTranslation();
+	const deleteMutation = useDeleteMutation();
+	const coilsQueryInvalidate = useCoilsQueryInvalidate();
 	return <QuickMenu {...props}>
 		<Menu.Item>
 			<CoilPreviewButton coil={coil}/>
@@ -22,6 +27,23 @@ export const CoilQuickMenu: FC<ICoilQuickMenuProps> = ({coil, ...props}) => {
 		</Menu.Item>
 		<Menu.Item>
 			<CoilCloneButton coil={coil}/>
+		</Menu.Item>
+		<Menu.Divider/>
+		<Menu.Item>
+			<CoilDeleteButton
+				coil={coil}
+				onOk={setShow => {
+					deleteMutation.mutate({
+						id: coil.id,
+					}, {
+						onSuccess: () => {
+							message.success(t('lab.coil.deleted.success'))
+							coilsQueryInvalidate();
+						},
+					})
+					setShow(false);
+				}}
+			/>
 		</Menu.Item>
 	</QuickMenu>
 }
