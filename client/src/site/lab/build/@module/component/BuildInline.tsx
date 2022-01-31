863e3bc09c5e7d1f258d96c2cfcf573889c64da8
Marek Hanzal <marek.hanzal@x32.cz>
2022-01-31 23:21:11 +0100

    Common: Improved inline build

--- client/src/site/lab/build/@module/component/BuildInline.tsx
+++ client/src/site/lab/build/@module/component/BuildInline.tsx
@@ -1,9 +1,8 @@
 import {BuildDto} from "@/sdk/puff-smith/build/dto";
 import {FC} from "react";
-import {Divider, Space, Tooltip, Typography} from "antd";
+import {Divider, Space, Typography} from "antd";
 import {AtomizerIcon, CoilIcon} from "@/puff-smith";
 import {WireInline} from "@/puff-smith/site/lab/wire";
-import {useTranslation} from "react-i18next";
 import {ArrowsAltOutlined} from "@ant-design/icons";
 
 export interface IBuildInlineProps {
@@ -11,18 +10,23 @@ export interface IBuildInlineProps {
 }
 
 export const BuildInline: FC<IBuildInlineProps> = ({build}) => {
-	const {t} = useTranslation();
 	return <Space direction={'vertical'}>
-		<Space split={<Divider type={'vertical'}/>}>
-			<Tooltip title={t('lab.build.inline.atomizer.tooltip')}>
-				<Space>
-					<Space size={2}><AtomizerIcon/>{build.atomizer.name}</Space>
-					<Typography.Text type={'secondary'}>{build.atomizer.vendor.name}</Typography.Text>
+		<Space size={2} direction={'vertical'}>
+			<Space>
+				<Space size={2}><AtomizerIcon/>{build.atomizer.name}</Space>
+				<Typography.Text type={'secondary'}>{build.atomizer.vendor.name}</Typography.Text>
+			</Space>
+			<Space split={<Divider type={'vertical'}/>}>
+				<Space size={2}>
+					<CoilIcon/>{build.coil.wraps}
 				</Space>
-			</Tooltip>
-			<Tooltip title={t('lab.build.inline.wraps.tooltip')}><Space size={2}><CoilIcon/>{build.coil.wraps}</Space></Tooltip>
-			<Space size={2}>{build.coil.size.toFixed(2)}<ArrowsAltOutlined/></Space>
-			<Space size={2}>{build.ohm.toFixed(2)}ohm</Space>
+				<Space size={2}>
+					{build.coil.size.toFixed(2)}<ArrowsAltOutlined/>
+				</Space>
+				<Space size={2}>
+					{build.ohm.toFixed(2)}ohm
+				</Space>
+			</Space>
 		</Space>
 		<WireInline wire={build.coil.wire}/>
 	</Space>
