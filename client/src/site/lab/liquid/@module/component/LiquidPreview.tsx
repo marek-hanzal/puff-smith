88626272b157e26cd3c5c469cff6e3fa7197dd97
Marek Hanzal <marek.hanzal@x32.cz>
2022-02-01 15:39:37 +0100

    Common: Improved UI a bit II

--- client/src/site/lab/liquid/@module/component/LiquidPreview.tsx
+++ client/src/site/lab/liquid/@module/component/LiquidPreview.tsx
@@ -1,35 +1,40 @@
 import {LiquidDto} from "@/sdk/puff-smith/liquid/dto";
 import {IPreviewProps, Preview} from "@leight-core/leight";
 import {FC} from "react";
-import {Divider} from "antd";
+import {Tabs} from "antd";
 import {CommentsSource, useCommentsQueryInvalidate} from "@/sdk/puff-smith/api/lab/liquid/comment/endpoint";
 import {CommentList} from "@/puff-smith/site/lab/comment";
 import {CreateCommentForm, LiquidInline} from "@/puff-smith/site/lab/liquid";
+import {useTranslation} from "react-i18next";
 
 export interface ILiquidPreviewProps extends Partial<IPreviewProps> {
 	liquid: LiquidDto
 }
 
 export const LiquidPreview: FC<ILiquidPreviewProps> = ({liquid, ...props}) => {
+	const {t} = useTranslation();
 	const commentsQueryInvalidate = useCommentsQueryInvalidate();
-	return <>
-		<Preview translation={'lab.liquid.preview'} {...props}>
-			{{
-				"name": <LiquidInline liquid={liquid}/>,
-				'pgvg': <>{liquid.pg}/{liquid.vg}</>,
-				'volume': liquid.volume + 'ml',
-			}}
-		</Preview>
-		<Divider/>
-		<CommentsSource
-			filter={{liquidId: liquid.id}}
-			defaultOrderBy={{stamp: false}}
-		>
-			<CommentList
-				form={<CreateCommentForm liquid={liquid}/>}
-				onEdit={() => commentsQueryInvalidate()}
-				onDelete={() => commentsQueryInvalidate()}
-			/>
-		</CommentsSource>
-	</>
+	return <Tabs>
+		<Tabs.TabPane key={'common'} tab={t('lab.liquid.common.tab')}>
+			<Preview translation={'lab.liquid.preview'} {...props}>
+				{{
+					"name": <LiquidInline liquid={liquid}/>,
+					'pgvg': <>{liquid.pg}/{liquid.vg}</>,
+					'volume': liquid.volume + 'ml',
+				}}
+			</Preview>
+		</Tabs.TabPane>
+		<Tabs.TabPane key={'comments'} tab={t('lab.liquid.comments.tab')}>
+			<CommentsSource
+				filter={{liquidId: liquid.id}}
+				defaultOrderBy={{stamp: false}}
+			>
+				<CommentList
+					form={<CreateCommentForm liquid={liquid}/>}
+					onEdit={() => commentsQueryInvalidate()}
+					onDelete={() => commentsQueryInvalidate()}
+				/>
+			</CommentsSource>
+		</Tabs.TabPane>
+	</Tabs>
 }
--- client/src/site/lab/mixture/@module/component/MixturePreview.tsx
+++ client/src/site/lab/mixture/@module/component/MixturePreview.tsx
@@ -2,49 +2,54 @@ import {MixtureDto} from "@/sdk/puff-smith/mixture/dto";
 import {asDayjs, Preview, PreviewBool, toLocalDate} from "@leight-core/leight";
 import {FC} from "react";
 import {LiquidInline} from "@/puff-smith/site/lab/liquid";
-import {Divider, Space, Typography} from "antd";
+import {Space, Tabs, Typography} from "antd";
 import {BaseInline} from "@/puff-smith/site/lab/base";
 import {BoosterInline} from "@/puff-smith/site/lab/booster";
 import {CreateCommentForm, MixtureAge, MixtureSteeping} from "@/puff-smith/site/lab/mixture";
 import {CommentsSource, useCommentsQueryInvalidate} from "@/sdk/puff-smith/api/lab/mixture/comment/endpoint";
 import {CommentList} from "@/puff-smith/site/lab/comment";
+import {useTranslation} from "react-i18next";
 
 export interface IMixturePreviewProps {
 	mixture: MixtureDto
 }
 
 export const MixturePreview: FC<IMixturePreviewProps> = ({mixture}) => {
+	const {t} = useTranslation();
 	const commentsQueryInvalidate = useCommentsQueryInvalidate();
-	return <>
-		<Preview translation={'lab.mixture.preview'}>
-			{{
-				"name": <Space>
-					<span>{mixture.name}</span>
-					<Typography.Text type={'secondary'}>{mixture.code}</Typography.Text>
-				</Space>,
-				"liquid": <LiquidInline liquid={mixture.liquid}/>,
-				"active": <PreviewBool bool={mixture.active}/>,
-				"base": <BaseInline base={mixture.base}/>,
-				"booster": <BoosterInline booster={mixture.booster}/>,
-				"pgvg": <span><span>{mixture.pg}</span>/<span>{mixture.vg}</span></span>,
-				"nicotine": mixture.nicotine + 'mg',
-				"age": <MixtureAge mixture={mixture}/>,
-				"steep": <MixtureSteeping mixture={mixture}/>,
-				"mixed": toLocalDate(mixture.mixed),
-				"expires": asDayjs(mixture.expires)?.format('MMMM YYYY'),
-				"volume": mixture.volume + 'ml',
-			}}
-		</Preview>
-		<Divider/>
-		<CommentsSource
-			filter={{mixtureId: mixture.id}}
-			defaultOrderBy={{stamp: false}}
-		>
-			<CommentList
-				form={<CreateCommentForm mixture={mixture}/>}
-				onEdit={() => commentsQueryInvalidate()}
-				onDelete={() => commentsQueryInvalidate()}
-			/>
-		</CommentsSource>
-	</>
+	return <Tabs>
+		<Tabs.TabPane key={'common'} tab={t('lab.mixture.common.tab')}>
+			<Preview translation={'lab.mixture.preview'}>
+				{{
+					"name": <Space>
+						<span>{mixture.name}</span>
+						<Typography.Text type={'secondary'}>{mixture.code}</Typography.Text>
+					</Space>,
+					"liquid": <LiquidInline liquid={mixture.liquid}/>,
+					"active": <PreviewBool bool={mixture.active}/>,
+					"base": <BaseInline base={mixture.base}/>,
+					"booster": <BoosterInline booster={mixture.booster}/>,
+					"pgvg": <span><span>{mixture.pg}</span>/<span>{mixture.vg}</span></span>,
+					"nicotine": mixture.nicotine + 'mg',
+					"age": <MixtureAge mixture={mixture}/>,
+					"steep": <MixtureSteeping mixture={mixture}/>,
+					"mixed": toLocalDate(mixture.mixed),
+					"expires": asDayjs(mixture.expires)?.format('MMMM YYYY'),
+					"volume": mixture.volume + 'ml',
+				}}
+			</Preview>
+		</Tabs.TabPane>
+		<Tabs.TabPane key={'comments'} tab={t('lab.mixture.comments.tab')}>
+			<CommentsSource
+				filter={{mixtureId: mixture.id}}
+				defaultOrderBy={{stamp: false}}
+			>
+				<CommentList
+					form={<CreateCommentForm mixture={mixture}/>}
+					onEdit={() => commentsQueryInvalidate()}
+					onDelete={() => commentsQueryInvalidate()}
+				/>
+			</CommentsSource>
+		</Tabs.TabPane>
+	</Tabs>
 }
