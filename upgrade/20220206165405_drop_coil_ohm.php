c1805f1603fed72dc3bb846f9068d312335f2abe
Marek Hanzal <marek.hanzal@x32.cz>
2022-02-06 18:59:20 +0100

    Common: Recovered some data

--- upgrade/20220206165405_drop_coil_ohm.php
+++ upgrade/20220206165405_drop_coil_ohm.php
@@ -2,9 +2,22 @@
 declare(strict_types=1);
 
 use Edde\Phinx\CommonMigration;
+use PuffSmith\Build\Repository\BuildRepositoryTrait;
+use PuffSmith\Coil\Repository\CoilRepositoryTrait;
 
 final class DropCoilOhm extends CommonMigration {
+	use BuildRepositoryTrait;
+	use CoilRepositoryTrait;
+
 	public function change(): void {
+		foreach ($this->buildRepository->all() as $build) {
+			$coil = $this->coilRepository->find($build->coil_id);
+			$this->buildRepository->change([
+				'id'  => $build->id,
+				'ohm' => $coil->ohm,
+			]);
+		}
+
 		$this
 			->table('z_coil')
 			->dropForeignKey(['wire_id'])
