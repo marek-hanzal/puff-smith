03034afe1f1ab76fc7de8f6fe7a754b44ea76b7c
Marek Hanzal <marek.hanzal@x32.cz>
2022-02-07 15:48:09 +0100

    Common: Fixed vape display

--- client/src/site/lab/vape/@module/table/VapeListItem.tsx
+++ client/src/site/lab/vape/@module/table/VapeListItem.tsx
@@ -3,7 +3,7 @@ import {ListItemProps} from "antd/lib/list";
 import {VapeDto} from "@/sdk/puff-smith/vape/dto";
 import {List} from "antd";
 import {VapePreviewButton, VapeQuickMenu} from "@/puff-smith/site/lab/vape";
-import {durationOf, toLocalDateTime} from "@leight-core/leight/dist";
+import {durationOf, toLocalDate} from "@leight-core/leight/dist";
 
 export interface IVapeListItemProps extends Partial<ListItemProps> {
 	vape: VapeDto;
@@ -15,9 +15,13 @@ export const VapeListItem: FC<IVapeListItemProps> = ({vape, ...props}) => {
 		{...props}
 	>
 		<List.Item.Meta
-			title={<VapePreviewButton style={{padding: 0}} title={vape.build.atomizer.name} vape={vape}/>}
+			title={<VapePreviewButton
+				style={{padding: 0}}
+				title={vape.build.atomizer.name}
+				vape={vape}
+			/>}
 			description={vape.mixture.liquid.name}
 		/>
-		{toLocalDateTime(vape.stamp) + ' (' + durationOf(vape.stamp).humanize() + ')'}
+		<span>{toLocalDate(vape.stamp) + ' (' + durationOf(vape.stamp).humanize() + ')'}</span>
 	</List.Item>;
 }
