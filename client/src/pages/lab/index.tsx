7eea3c88be9983701937e4a351266338614ad9b5
Marek Hanzal <marek.hanzal@x32.cz>
2022-02-11 22:24:37 +0100

    Common: A little update

--- client/src/pages/lab/index.tsx
+++ client/src/pages/lab/index.tsx
@@ -1,7 +1,6 @@
 import {LabMenu, LabPage, withLabLayout} from "@/puff-smith/site/lab";
 import {FullLogoIcon, LiquidIcon, LogoIcon} from "@/puff-smith";
 import {Template, useIsMobile} from "@leight-core/leight";
-import {BrowserView, MobileView} from "react-device-detect";
 
 export default withLabLayout(function Index() {
 	const isMobile = useIsMobile();
@@ -13,14 +12,11 @@ export default withLabLayout(function Index() {
 		extraMobile={<LogoIcon style={{width: '7.5em'}}/>}
 		headerProps={{style: isMobile ? {padding: "8px 12px"} : undefined}}
 	>
-		<BrowserView>
+		{isMobile ?
+			<LabMenu/> :
 			<Template
 				icon={<FullLogoIcon style={{width: "20vw", maxWidth: "30em"}}/>}
 				label={"lab.home"}
-			/>
-		</BrowserView>
-		<MobileView>
-			<LabMenu/>
-		</MobileView>
+			/>}
 	</LabPage>;
 });
--- client/src/site/shared/@module/layout/@module/AppLayout.tsx
+++ client/src/site/shared/@module/layout/@module/AppLayout.tsx
@@ -1,4 +1,4 @@
-import {App, createQueryClient, IAppProps, IPageWithLayout, useIsMobile} from "@leight-core/leight";
+import {App, createQueryClient, IAppProps, IPageWithLayout} from "@leight-core/leight";
 import dayjs from "dayjs";
 import i18next from "i18next";
 import {FC} from "react";
@@ -11,11 +11,10 @@ export interface IAppLayoutProps extends Partial<IAppProps> {
 
 export const AppLayout: FC<IAppLayoutProps> = props => {
 	// useQueryPersistence(queryClient, "puff-smith");
-	const isMobile = useIsMobile();
 	return <App
 		clientLink={process.env.NEXT_PUBLIC_PUBLIC_URL + "/puff-smith/client.json"}
 		queryClient={queryClient}
-		logo={<FullLogoIcon style={isMobile ? {width: "90vw"} : {width: "10vw"}}/>}
+		logo={<FullLogoIcon/>}
 		dayjs={dayjs}
 		i18next={i18next}
 		{...props}
