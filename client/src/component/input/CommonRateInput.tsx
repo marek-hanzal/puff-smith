11e922922fa96dfa36ee3e2187faf13f6e9a2559
Marek Hanzal <marek.hanzal@x32.cz>
2022-02-12 19:19:17 +0100

    Common: A little fix

--- client/src/component/input/CommonRateInput.tsx
+++ client/src/component/input/CommonRateInput.tsx
@@ -2,15 +2,14 @@ import {FC, forwardRef} from "react";
 import {Rate, RateProps} from "antd";
 
 export interface ICommonRateInputProps extends Partial<Omit<RateProps, "value">> {
-	value?: number | null
+	value?: number | null;
 }
 
-export const CommonRateInput: FC<ICommonRateInputProps> = forwardRef(({value, ...props}, ref) => {
+export const CommonRateInput: FC<ICommonRateInputProps> = forwardRef((props, ref) => {
 	return <Rate
 		count={9}
 		ref={ref as any}
-		value={value || undefined}
 		allowClear
-		{...props}
+		{...props as any}
 	/>
 });
