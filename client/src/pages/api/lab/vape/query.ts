5894e334c3fd4514ddc506f53af3fcac65c18e42
Marek Hanzal <marek.hanzal@x32.cz>
2022-02-20 18:30:32 +0100

    Common: ...

--- client/src/pages/api/lab/vape/query.ts
+++ client/src/pages/api/lab/vape/query.ts
@@ -1,8 +1,9 @@
 import {NextApiRequest, NextApiResponse} from "next";
 import {createPrismaClient} from "@/sdk/prisma";
+import {IQueryApiResponse} from "@/puff-smith/api/lab/vape";
 
 const prisma = createPrismaClient();
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
+export default async (req: NextApiRequest, res: NextApiResponse<IQueryApiResponse<any>>) => {
 	res.status(200).json(await prisma.z_vape.findMany(req.body));
 }
