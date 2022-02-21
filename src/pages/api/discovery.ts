f0eb30ada627acd8e3dac072585be7977a285b86
Marek Hanzal <marek.hanzal@x32.cz>
2022-02-21 18:42:42 +0100

    Common: Soo, let's start

--- src/pages/api/discovery.ts
+++ src/pages/api/discovery.ts
@@ -2,6 +2,9 @@ import type {NextApiRequest, NextApiResponse} from 'next'
 import {IDiscovery} from "@leight-core/leight/dist";
 
 export default (req: NextApiRequest, res: NextApiResponse<IDiscovery>) => {
+	// glob.sync('src/pages/api/**/*.ts').forEach(source => {
+	// 	const file = ts.createSourceFile(source, fs.readFileSync(source, 'utf8'), ts.ScriptTarget.Latest);
+	// })
 	res.status(200).json({
 		index: {},
 	})
