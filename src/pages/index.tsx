6ff38bdb975c8ae3268a1bf88b0fb09a387bb4fe
Marek Hanzal <marek.hanzal@x32.cz>
2022-02-26 16:15:52 +0100

    Common: Improved index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,14 @@
-import {HomeOutlined} from "@ant-design/icons";
-import {LoaderLayout} from "@leight-core/common";
-import {useEffect} from "react";
-import {useNavigate} from "@leight-core/utils";
-import {withAppLayout} from "@/puff-smith/site/shared";
-import {FullLogoIcon} from "@/puff-smith";
 import {GetServerSideProps} from "next";
 import {getSession} from "next-auth/react";
-import {Session} from "next-auth";
 
-interface IIndexProps {
-	_session: Session | null;
-}
+export default function Index() {
+	return null;
+};
 
-export default withAppLayout(function Index({_session}: IIndexProps) {
-	const navigate = useNavigate();
-	useEffect(() => {
-		setTimeout(() => {
-			navigate("/" + "public");
-		}, 1500);
-	}, []);
-	return <LoaderLayout
-		logo={<FullLogoIcon/>}
-		icon={<HomeOutlined/>}
-	/>;
-});
-
-export const getServerSideProps: GetServerSideProps<IIndexProps> = async ctx => {
+export const getServerSideProps: GetServerSideProps = async ctx => {
+	const session = await getSession({ctx});
 	return {
-		props: {
-			_session: await getSession({ctx}),
-		},
+		redirect: session ? {destination: '/lab'} : {destination: '/public'},
+		props: {},
 	}
 }
