aa763492c041c67c288b981332a5d5aa7b36c81b
Marek Hanzal <marek.hanzal@x32.cz>
2022-02-26 15:40:00 +0100

    Common: Improved auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,62 @@
 import NextAuth from "next-auth"
 import {createPrismaClient} from "@/puff-smith/prisma";
 import {PrismaAdapter} from "@next-auth/prisma-adapter";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 const prisma = createPrismaClient();
 
 export default NextAuth({
 	adapter: PrismaAdapter(prisma),
 	providers: [
+		CredentialsProvider({
+			name: 'credential',
+			credentials: {
+				username: {
+					label: 'Email',
+					type: 'text',
+					placeholder: 'john.doe@example.com',
+				},
+				password: {
+					label: 'Password',
+					type: 'password',
+				},
+			},
+			authorize: credentials => {
+				/**
+				 * @TODO prisma lookup here
+				 */
+				return credentials?.username === 'test' && credentials?.password === '1234' ? {
+					id: '1234',
+					name: 'test',
+					email: 'test@example.com',
+				} : null;
+			},
+		}),
+
 		// https://next-auth.js.org/providers/google
 		// https://console.developers.google.com/apis/credentials?project=puff-smith&supportedpurview=project
 	],
+	callbacks: {
+		jwt: ({token, user}) => {
+			if (!user) {
+				return token;
+			}
+			token.id = user.id;
+			return token;
+		},
+		session: ({session, token}) => {
+			if (!token) {
+				return session;
+			}
+			session.id = token.id;
+			return session;
+		},
+	},
+	/**
+	 * @TODO take a secret from ENV
+	 */
+	secret: '1234',
+	jwt: {
+		secret: '1234',
+	},
 })
