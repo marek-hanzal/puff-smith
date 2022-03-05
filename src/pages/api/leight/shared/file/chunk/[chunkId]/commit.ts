bab47cd075e412956a94dea342a1f91064d4adac
Marek Hanzal <marek.hanzal@x32.cz>
2022-03-05 01:16:30 +0100

    Common: Yahoo, files are saved in database!

--- src/pages/api/leight/shared/file/chunk/[chunkId]/commit.ts
+++ src/pages/api/leight/shared/file/chunk/[chunkId]/commit.ts
@@ -2,9 +2,19 @@ import {CommitChunkEndpoint, Endpoint, IChunkEndpointQuery} from "@leight-core/s
 import {IChunkCommit, IFile} from "@leight-core/api";
 import chunkService from "@/puff-smith/service/chunk";
 import {getToken} from "next-auth/jwt";
+import prismaClient from "@/puff-smith/service/prisma";
 
 export default Endpoint<"Commit", IChunkCommit, IFile, IChunkEndpointQuery>(CommitChunkEndpoint(chunkService, async (file, params) => {
 	const token: any = await getToken({req: params.req});
-	console.log('wanna persist, yaaay; user id', token.id);
+	await prismaClient.file.create({
+		data: {
+			...file,
+			user: {
+				connect: {
+					id: token.sub,
+				}
+			}
+		}
+	});
 	return file;
 }));
