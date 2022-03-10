d062c9855c1ad78736cea19dac3f434438eab422
Marek Hanzal <marek.hanzal@x32.cz>
2022-03-10 14:13:16 +0100

    Common: ...

--- src/service/translation/prisma.ts
+++ src/service/translation/prisma.ts
@@ -1,7 +1,6 @@
 import prismaClient from "@/puff-smith/service/prisma";
 import {ITranslationCreate} from "@/puff-smith/service/translation";
 import {sha256} from "@/puff-smith/service/sha256";
-import {PrismaClientKnownRequestError} from "@prisma/client/runtime";
 
 export async function translationCreate(translation: ITranslationCreate) {
 	const hash = sha256(translation.label);
@@ -13,7 +12,7 @@ export async function translationCreate(translation: ITranslationCreate) {
 			}
 		})
 	} catch (e: any) {
-		if ((e as PrismaClientKnownRequestError)?.message?.includes('Unique constraint failed on the fields')) {
+		if ((e as Error)?.message?.includes('Unique constraint failed on the fields')) {
 			return await prismaClient.translation.update({
 				where: {
 					language_hash: {
