05d118478a3ee90a44c9a5c909974b18f840257d
Marek Hanzal <marek.hanzal@x32.cz>
2022-03-17 13:35:34 +0100

    Common: Added production data backup

--- src/site/root/job/@module/menu/JobMenu.tsx
+++ src/site/root/job/@module/menu/JobMenu.tsx
@@ -1,17 +1,17 @@
 import {FC} from "react";
-import {CreateMenuItem, IMenuProps, Menu} from "@leight-core/client";
-import {JobIcon} from "@/puff-smith";
+import {CreateMenuItem, IMenuProps, ListIcon, Menu} from "@leight-core/client";
+import {CheckCircleOutlined, CloseCircleOutlined, IssuesCloseOutlined, SmileOutlined, SyncOutlined} from "@ant-design/icons";
 
 export interface IJobMenuProps extends Partial<IMenuProps> {
 }
 
 export const JobMenu: FC<IJobMenuProps> = props => {
 	return <Menu style={{border: 'none'}} mode={'horizontal'} {...props}>
-		{CreateMenuItem('common.job.status.RUNNING.tab', '/root/job/running', <JobIcon/>)}
-		{CreateMenuItem('common.job.status.REVIEW.tab', '/root/job/review', <JobIcon/>)}
-		{CreateMenuItem('common.job.status.FAILURE.tab', '/root/job/failure', <JobIcon/>)}
-		{CreateMenuItem('common.job.status.SUCCESS.tab', '/root/job/success', <JobIcon/>)}
-		{CreateMenuItem('common.job.status.DONE.tab', '/root/job/done', <JobIcon/>)}
-		{CreateMenuItem('common.job.status.ALL.tab', '/root/job/all', <JobIcon/>)}
+		{CreateMenuItem('common.job.status.RUNNING.tab', '/root/job/running', <SyncOutlined/>)}
+		{CreateMenuItem('common.job.status.REVIEW.tab', '/root/job/review', <IssuesCloseOutlined/>)}
+		{CreateMenuItem('common.job.status.FAILURE.tab', '/root/job/failure', <CloseCircleOutlined/>)}
+		{CreateMenuItem('common.job.status.SUCCESS.tab', '/root/job/success', <SmileOutlined/>)}
+		{CreateMenuItem('common.job.status.DONE.tab', '/root/job/done', <CheckCircleOutlined/>)}
+		{CreateMenuItem('common.job.status.ALL.tab', '/root/job/all', <ListIcon/>)}
 	</Menu>
 }
