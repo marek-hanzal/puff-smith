0d1549ea7dfe85874c5bc756799e0df04434c0bf
Marek Hanzal <marek.hanzal@x32.cz>
2022-03-25 04:09:04 +0100

    Common: ...

---
+++ src/pages/market/aroma/index.tsx
@@ -0,0 +1,22 @@
+import {MarketPage, withMarketLayout} from "@/puff-smith/site/market";
+import {AromasList} from "@/puff-smith/site/market/aroma";
+import {AromasSourceControlProvider} from "@/sdk/api/aroma/query";
+import {LiquidIcon} from "@/puff-smith";
+
+export default withMarketLayout(function Index() {
+	return <MarketPage
+		title={"market.aroma.index"}
+		menuSelection={['/market/aroma']}
+		icon={<LiquidIcon/>}
+	>
+		<AromasSourceControlProvider
+			defaultOrderBy={{
+				name: 'asc',
+			}}
+			defaultPage={0}
+			defaultSize={5}
+		>
+			<AromasList/>
+		</AromasSourceControlProvider>
+	</MarketPage>;
+});
---
+++ src/pages/market/base/index.tsx
@@ -0,0 +1,22 @@
+import {MarketPage, withMarketLayout} from "@/puff-smith/site/market";
+import {BasesList} from "@/puff-smith/site/market/base";
+import {BasesSourceControlProvider} from "@/sdk/api/base/query";
+import {BaseIcon} from "@/puff-smith";
+
+export default withMarketLayout(function Index() {
+	return <MarketPage
+		title={"market.base.index"}
+		menuSelection={['/market/base']}
+		icon={<BaseIcon/>}
+	>
+		<BasesSourceControlProvider
+			defaultOrderBy={{
+				name: 'asc',
+			}}
+			defaultPage={0}
+			defaultSize={5}
+		>
+			<BasesList/>
+		</BasesSourceControlProvider>
+	</MarketPage>;
+});
