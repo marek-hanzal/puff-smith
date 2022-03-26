f4a54ee8f28c9d9e01835f83f46d9e63abeb9bc2
Marek Hanzal <marek.hanzal@x32.cz>
2022-03-26 22:03:07 +0100

    Common: Initial idea of Tariff and Price

---
+++ prisma/migrations/20220326210247_tariff/migration.sql
@@ -0,0 +1,33 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "tariffId" TEXT;
+
+-- CreateTable
+CREATE TABLE "Tariff" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "from" TIMESTAMP(3),
+    "to" TIMESTAMP(3),
+    "created" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Tariff_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Price" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "price" DECIMAL(10,2) NOT NULL,
+    "tariffId" TEXT NOT NULL,
+    "from" TIMESTAMP(3),
+    "to" TIMESTAMP(3),
+    "created" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Price_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_tariffId_fkey" FOREIGN KEY ("tariffId") REFERENCES "Tariff"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Price" ADD CONSTRAINT "Price_tariffId_fkey" FOREIGN KEY ("tariffId") REFERENCES "Tariff"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
