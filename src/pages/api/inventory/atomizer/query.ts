d98b69f982095f1ac6e6cd1f7b036cb21dd0bea5
Marek Hanzal <marek.hanzal@x32.cz>
2022-03-26 01:09:02 +0100

    Common: Preparing yet another refacotring

---
+++ src/pages/api/inventory/atomizer/query.ts
@@ -0,0 +1,4 @@
+import {QueryEndpoint} from "@leight-core/server";
+import {AtomizerService, IAtomizer, IAtomizerQuery} from "@/puff-smith/service/atomizer";
+
+export default QueryEndpoint<"Atomizers", IAtomizerQuery, IAtomizer>(AtomizerService().handleQuery);
