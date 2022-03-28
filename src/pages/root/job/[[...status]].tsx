f1e0b8b944836e739ceec31eeab14b0740d7bc4c
Marek Hanzal <marek.hanzal@x32.cz>
2022-03-28 17:17:02 +0200

    Common: A little nice improvement

--- src/pages/root/job/[[...status]].tsx
+++ src/pages/root/job/[[...status]].tsx
@@ -3,78 +3,92 @@ import {IJobListProps, JobsList} from "@/puff-smith/site/shared/job";
 import {JobsSourceControlProvider} from "@/sdk/api/job/query";
 import {IJobStatus} from "@leight-core/api";
 import {JobMenu} from "@/puff-smith/site/root/job";
-import {useParams} from "@leight-core/client";
+import {useNavigate, useParams} from "@leight-core/client";
 import {JobIcon} from "@/puff-smith";
+import {message} from "antd";
+import {useTranslation} from "react-i18next";
 
 interface IJobConfig {
 	filter?: IJobStatus[];
 	listProps?: Partial<IJobListProps>;
 }
 
-const configs: IJobConfigObject = {
-	'running': {
-		filter: ['RUNNING', 'NEW'],
-		listProps: {
-			sourceProps: {
-				live: 1000,
-			},
-			disableToolbar: true,
-		},
-	},
-	'review': {
-		filter: ['REVIEW'],
-		listProps: {
-			sourceProps: {
-				live: 0,
-			},
-			showFilter: false,
-		},
-	},
-	'failure': {
-		filter: ['FAILURE'],
-		listProps: {
-			sourceProps: {
-				live: 0,
-			},
-			showFilter: false,
-		},
-	},
-	'success': {
-		filter: ['SUCCESS'],
-		listProps: {
-			sourceProps: {
-				live: 0,
-			},
-			showFilter: false,
-		},
-	},
-	'done': {
-		filter: ['DONE'],
-		listProps: {
-			sourceProps: {
-				live: 0,
-			},
-			showFilter: false,
-			showCommit: false,
-		},
-	},
-	'all': {
-		filter: undefined,
-		listProps: {
-			sourceProps: {
-				live: 5000,
-			},
-			showCommit: false,
-		},
-	},
-};
-
 interface IJobConfigObject {
 	[index: string]: IJobConfig;
 }
 
 export default withRootLayout(function Index() {
+	const {t} = useTranslation();
 	const {status, name} = useParams();
+	const navigate = useNavigate();
+	const configs: IJobConfigObject = {
+		'running': {
+			filter: ['RUNNING', 'NEW'],
+			listProps: {
+				sourceProps: {
+					live: 1000,
+					options: {
+						onSuccess: async data => {
+							if (!data.count) {
+								navigate('/root/job/all');
+								await message.success({
+									content: t('common.job.all.done'),
+									key: 'common.job.all.done',
+								})
+							}
+						}
+					}
+				},
+				disableToolbar: true,
+			},
+		},
+		'review': {
+			filter: ['REVIEW'],
+			listProps: {
+				sourceProps: {
+					live: 0,
+				},
+				showFilter: false,
+			},
+		},
+		'failure': {
+			filter: ['FAILURE'],
+			listProps: {
+				sourceProps: {
+					live: 0,
+				},
+				showFilter: false,
+			},
+		},
+		'success': {
+			filter: ['SUCCESS'],
+			listProps: {
+				sourceProps: {
+					live: 0,
+				},
+				showFilter: false,
+			},
+		},
+		'done': {
+			filter: ['DONE'],
+			listProps: {
+				sourceProps: {
+					live: 0,
+				},
+				showFilter: false,
+				showCommit: false,
+			},
+		},
+		'all': {
+			filter: undefined,
+			listProps: {
+				sourceProps: {
+					live: 5000,
+				},
+				showCommit: false,
+			},
+		},
+	};
 	const config = configs[status || 'running'];
 	return <RootPage
 		title={"root.job"}
