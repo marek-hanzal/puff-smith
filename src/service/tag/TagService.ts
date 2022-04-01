8722234458c4899273b0e4beeb9486afe5ffd518
Marek Hanzal <marek.hanzal@x32.cz>
2022-04-01 21:04:09 +0200

    Common: Refactoring repository services IV

--- src/service/tag/TagService.ts
+++ src/service/tag/TagService.ts
@@ -1,51 +1,39 @@
 import {IPrismaClientTransaction} from "@leight-core/api";
 import {ITagService} from "@/puff-smith/service/tag/interface";
-import {AbstractRepositoryService, handleUniqueException} from "@leight-core/server";
+import {RepositoryService} from "@leight-core/server";
 import prisma from "@/puff-smith/service/prisma";
 import {Tag} from "@prisma/client";
 
-export const TagService = (prismaClient: IPrismaClientTransaction = prisma): ITagService => {
-	const service: ITagService = {
-		...AbstractRepositoryService<ITagService>(prismaClient, prismaClient.tag, async tag => tag),
-		async handleCreate({request}) {
-			return service.map(await service.create(request));
-		},
-		create: async tag => {
-			try {
-				return await prismaClient.tag.create({
-					data: {
-						...tag,
-						code: `${tag.code}`,
-					},
-				})
-			} catch (e) {
-				return handleUniqueException(e, async () => {
-					return prismaClient.tag.update({
-						where: {
-							code_group: {
-								code: `${tag.code}`,
-								group: tag.group,
-							},
-						},
-						data: tag,
-					})
-				});
-			}
-		},
-		importers: () => ({
-			tag: () => ({
-				handler: service.create,
-			}),
+export const TagService = (prismaClient: IPrismaClientTransaction = prisma): ITagService => ({
+	...RepositoryService<ITagService>({
+		name: 'tag',
+		source: prismaClient.tag,
+		mapper: async tag => tag,
+		create: async tag => prismaClient.tag.create({
+			data: {
+				...tag,
+				code: `${tag.code}`,
+			},
 		}),
-		async fetchCodes(codes, group) {
-			return (await Promise.all(codes.split(/,\s+/ig).map(async code => await prismaClient.tag.findFirst({
-				where: {
-					code: `${code}`.toLowerCase(),
-					group,
-				}
-			})))).filter(tag => tag !== null) as Tag[];
-		}
-	};
-
-	return service;
-};
+		onUnique: async create => prismaClient.tag.update({
+			where: {
+				id: (await prismaClient.tag.findFirst({
+					where: {
+						code: `${create.code}`,
+						group: create.group,
+					},
+					rejectOnNotFound: true,
+				})).id,
+			},
+			data: create,
+		}),
+	}),
+	async fetchCodes(codes, group) {
+		return (await Promise.all(codes.split(/,\s+/ig).map(async code => await prismaClient.tag.findFirst({
+			where: {
+				code: `${code}`.toLowerCase(),
+				group,
+			}
+		})))).filter(tag => tag !== null) as Tag[];
+	}
+})
