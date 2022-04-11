1a0b252984a6bf365aeaafffd52047215ec6c90f
Marek Hanzal <marek.hanzal@x32.cz>
2022-04-11 17:54:10 +0200

    Common: Improving Prisma logging

--- src/service/prisma.ts
+++ src/service/prisma.ts
@@ -1,9 +1,32 @@
+import {Logger} from "@leight-core/server";
 import {PrismaClient} from "@prisma/client";
 
 const createPrismaClient = () => {
-	return new PrismaClient({
-		log: ["info", "warn", "error"],
+	const prisma = new PrismaClient({
+		rejectOnNotFound: true,
+		errorFormat: "pretty",
+		log: [
+			{
+				emit: "event",
+				level: "query",
+			},
+			{
+				emit: "stdout",
+				level: "error",
+			},
+			{
+				emit: "stdout",
+				level: "info",
+			},
+			{
+				emit: "stdout",
+				level: "warn",
+			},
+		],
 	});
+	prisma.$on("query", ({query, params, duration}) => Logger("query").debug("Query", {query, params, duration}));
+	// prisma.$on("error", (e => Logger("query").error("Query Error", {error: e}));
+	return prisma;
 };
 
 let prisma: PrismaClient;
