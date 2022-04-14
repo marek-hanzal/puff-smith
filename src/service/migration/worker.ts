47a14abed448012add77722bb82ceb08533d6adf
Marek Hanzal <marek.hanzal@x32.cz>
2022-04-14 16:17:01 +0200

    Common: (basic) Migration is somehow working

--- src/service/migration/worker.ts
+++ src/service/migration/worker.ts
@@ -2,9 +2,11 @@ import {Agenda} from "@/puff-smith/agenda/agenda";
 import {MigrateJobName} from "@/puff-smith/agenda/job/migrate";
 import {Logger} from "@leight-core/server";
 
-(async () => {
+const worker = async () => {
 	const logger = Logger("migration");
 	logger.info("Scheduling migration job");
 	await (await Agenda()).now(MigrateJobName, {});
 	logger.info("Done");
-})();
+};
+
+worker().then(() => console.log("Worker Done"));
