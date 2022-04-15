02703e9f1e1372fdc2e38c098652598a00a49149
Marek Hanzal <marek.hanzal@x32.cz>
2022-04-15 12:37:26 +0200

    Common: Fixed inline filtering

--- src/component/filter/InlineFilter.tsx
+++ src/component/filter/InlineFilter.tsx
@@ -2,7 +2,7 @@ import {IFilterGroup} from "@/puff-smith/component";
 import {IQuery, IQueryFilter} from "@leight-core/api";
 import {useFilterContext} from "@leight-core/client";
 import {Button, Divider, Radio, Space, Typography} from "antd";
-import {PropsWithChildren} from "react";
+import {PropsWithChildren, useState} from "react";
 import {useTranslation} from "react-i18next";
 
 export interface IInlineFilterProps<TQuery extends IQuery<any, any>> {
@@ -23,22 +23,29 @@ export const InlineFilter = <TQuery extends IQuery<any, any>>(
 	}: PropsWithChildren<IInlineFilterProps<TQuery>>) => {
 	const {t} = useTranslation();
 	const filterContext = useFilterContext();
+	const [state, setState] = useState<{ [index in string]: string | undefined }>();
 	const filter = fromFilter(filterContext.filter);
 	return <Space size={0} split={<Divider type={"vertical"}/>}>
 		{filters.map(group => <Space key={`filter-group-${group.name}`}>
 			<Typography.Text type={"secondary"}>{t(`${translation}.filter.${group.name}.label`)}</Typography.Text>
-			<Radio.Group size={"large"} value={`filter-${group.name}-${JSON.stringify(filter || {})}`}>
+			<Radio.Group size={"large"} value={state?.[group.name]}>
 				<Radio.Button
-					value={`filter-${group.name}-${JSON.stringify({})}`}
-					onClick={() => filterContext.mergeFilter(toFilter(group.reset))}
+					value={undefined}
+					onClick={() => {
+						filterContext.mergeFilter(toFilter(group.reset));
+						setState(state => ({...state, [group.name]: undefined}));
+					}}
 				>
 					{t(`${translation}.filter.${group.name}.reset`)}
 				</Radio.Button>
 				{group.options.map(filter => <Radio.Button
 					type={"text"}
-					onClick={() => filterContext.mergeFilter(toFilter(filter.filter))}
-					value={`filter-${group.name}-${JSON.stringify(filter.filter)}`}
-					key={`filter-${group.name}-${JSON.stringify(filter.filter)}`}
+					onClick={() => {
+						filterContext.mergeFilter(toFilter(filter.filter));
+						setState(state => ({...state, [group.name]: filter.name}));
+					}}
+					value={filter.name}
+					key={`filter-${group.name}-${filter.name}`}
 				>
 					{group.render?.(t(`${translation}.filter.${group.name}.${filter.name}.label`, filter.name)) || t(`${translation}.filter.${group.name}.${filter.name}.label`, filter.name)}
 				</Radio.Button>)}
@@ -47,7 +54,10 @@ export const InlineFilter = <TQuery extends IQuery<any, any>>(
 		<Button
 			type={"link"}
 			size={"large"}
-			onClick={() => filterContext.setFilter()}
+			onClick={() => {
+				filterContext.setFilter();
+				setState(undefined);
+			}}
 		>
 			{t("common.filter.clear")}
 		</Button>
