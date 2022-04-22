eaae9261684d2faebec08bac8b6b975b2737d9dc
Marek Hanzal <marek.hanzal@x32.cz>
2022-04-22 22:52:56 +0200

    Common: Prepared an idea of quick-mix and clever-mix

--- src/site/lab/liquid/@module/view/LiquidCreateView.tsx
+++ src/site/lab/liquid/@module/view/LiquidCreateView.tsx
@@ -3,12 +3,15 @@ import {LiquidQuickMixForm} from "@/puff-smith/site/lab/liquid";
 import {useCheckPrice} from "@/puff-smith/site/shared/price";
 import {DollarCircleOutlined} from "@ant-design/icons";
 import {Loader, Template} from "@leight-core/client";
+import {Divider, Tabs} from "antd";
 import {FC} from "react";
+import {useTranslation} from "react-i18next";
 
 export interface ILiquidCreateViewProps {
 }
 
 export const LiquidCreateView: FC<ILiquidCreateViewProps> = () => {
+	const {t} = useTranslation();
 	const checkPrice = useCheckPrice("lab.liquid.create");
 	return <Loader
 		icon={<LiquidIcon/>}
@@ -16,7 +19,25 @@ export const LiquidCreateView: FC<ILiquidCreateViewProps> = () => {
 		error={false}
 	>
 		{checkPrice.pass ?
-			<LiquidQuickMixForm/> :
+			<Tabs>
+				<Tabs.TabPane key={"quick-mix"} tab={t("lab.liquid.create.quick-mix.tab")}>
+					<Template
+						label={"lab.liquid.create.quick-mix"}
+						extra={<Divider/>}
+						span={22}
+					>
+						<LiquidQuickMixForm/>
+					</Template>
+				</Tabs.TabPane>
+				<Tabs.TabPane key={"clever-mix"} tab={t("lab.liquid.create.clever-mix.tab")}>
+					<Template
+						label={"lab.liquid.create.clever-mix"}
+						extra={<Divider/>}
+						span={22}
+					>
+					</Template>
+				</Tabs.TabPane>
+			</Tabs> :
 			<Template
 				icon={<DollarCircleOutlined/>}
 				status={"error"}
