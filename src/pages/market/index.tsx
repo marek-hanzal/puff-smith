93f8e52dea57bcef12843e626bec01551e1971c0
Marek Hanzal <marek.hanzal@x32.cz>
2022-04-26 00:30:34 +0200

    Common: ...

--- src/pages/market/index.tsx
+++ src/pages/market/index.tsx
@@ -1,8 +1,11 @@
-import {FullLogoIcon} from "@/puff-smith";
+import {FullLogoIcon, LiquidIcon} from "@/puff-smith";
 import {MarketPage, withMarketLayout} from "@/puff-smith/site/market";
+import {useMixtureUpdateMutation} from "@/sdk/api/mixture/update";
 import {HomeIcon, Template} from "@leight-core/client";
+import {Button} from "antd";
 
 export default withMarketLayout(function Index() {
+	const mixtureUpdateMutation = useMixtureUpdateMutation();
 	return <MarketPage
 		title={"market.index"}
 		menuSelection={["/market"]}
@@ -12,6 +15,13 @@ export default withMarketLayout(function Index() {
 			icon={<FullLogoIcon style={{width: "20vw", maxWidth: "30em"}}/>}
 			status={"info"}
 			label={"market.home"}
+			extra={<Button
+				size={"large"}
+				type={"primary"}
+				icon={<LiquidIcon/>}
+				onClick={() => mixtureUpdateMutation.mutate()}
+				loading={mixtureUpdateMutation.isLoading}
+			>Kaboom!</Button>}
 		/>
 	</MarketPage>;
 });
