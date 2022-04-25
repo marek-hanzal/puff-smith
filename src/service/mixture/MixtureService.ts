d97c0e1411fb9f0aa89f59acfc5b5664854a7e57
Marek Hanzal <marek.hanzal@x32.cz>
2022-04-25 17:38:31 +0200

    Common: Another mixture boilerplate

---
+++ src/service/mixture/MixtureService.ts
@@ -0,0 +1,30 @@
+import {AromaService} from "@/puff-smith/service/aroma";
+import {IMixtureService} from "@/puff-smith/service/mixture";
+import prisma from "@/puff-smith/service/prisma";
+import {IPrismaClientTransaction} from "@leight-core/api";
+import {RepositoryService} from "@leight-core/server";
+
+export const MixtureService = (prismaClient: IPrismaClientTransaction = prisma) => RepositoryService<IMixtureService>({
+	name: "mixture",
+	source: prismaClient.mixture,
+	mapper: async mixture => {
+		const aroma = await AromaService(prismaClient).toMap(mixture.aromaId);
+		return {
+			...mixture,
+			content: mixture.content.toNumber(),
+			diff: mixture.diff.toNumber(),
+			nicotine: mixture.nicotine.toNumber(),
+			vg: mixture.vg.toNumber(),
+			pg: mixture.pg.toNumber(),
+			vgToMl: mixture.vgToMl.toNumber(),
+			pgToMl: mixture.pgToMl.toNumber(),
+			aroma,
+			volume: aroma.volume || 0,
+		};
+	},
+	create: async mixture => prismaClient.mixture.create({
+		data: {
+			...mixture,
+		},
+	}),
+});
---
+++ src/service/mixture/interface.ts
@@ -0,0 +1,56 @@
+import {IAroma} from "@/puff-smith/service/aroma";
+import {IBase} from "@/puff-smith/service/base";
+import {IBooster} from "@/puff-smith/service/booster";
+import {IQuery, IRepositoryService} from "@leight-core/api";
+import {Mixture, Prisma} from "@prisma/client";
+import {ParsedUrlQuery} from "querystring";
+
+export type IMixtureError = "LESS" | "MORE";
+
+export interface IMixtureCreate {
+	aromaId: string;
+	boosterId?: string;
+	baseId?: string;
+	content: number;
+	volume: number;
+	diff: number;
+	nicotine: number;
+	vg: number;
+	pg: number;
+	vgToMl: number;
+	pgToMl: number;
+	error?: IMixtureError;
+}
+
+export interface IMixtureQuery extends IQuery<Prisma.MixtureWhereInput, Prisma.MixtureOrderByWithRelationInput> {
+}
+
+export interface IMixture {
+	id: string;
+	content: number;
+	volume: number;
+	diff: number;
+	nicotine: number;
+	vg: number;
+	pg: number;
+	vgToMl: number;
+	pgToMl: number;
+	aromaId: string;
+	aroma: IAroma;
+	boosterId?: string | null;
+	booster?: IBooster | null;
+	baseId?: string | null;
+	base?: IBase | null;
+	error?: IMixtureError | null;
+}
+
+export interface IMixtureFetchProps {
+	mixture: IMixture;
+}
+
+export interface IMixtureFetchQuery extends ParsedUrlQuery {
+	mixtureId: string;
+}
+
+export interface IMixtureService extends IRepositoryService<IMixtureCreate, Mixture, IMixture, IMixtureQuery, IMixtureFetchProps, IMixtureFetchQuery> {
+}
