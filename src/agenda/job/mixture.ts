e1091d3d64f90b8a3b3a8850614f7d117ff01e17
Marek Hanzal <marek.hanzal@x32.cz>
2022-04-27 00:23:16 +0200

    Common: Experiment with faster mixture generation

--- src/agenda/job/mixture.ts
+++ src/agenda/job/mixture.ts
@@ -1,4 +1,5 @@
 import {JobService} from "@/puff-smith/service/job/JobService";
+import {IMixtureCreate} from "@/puff-smith/service/mixture/interface";
 import {MixtureService} from "@/puff-smith/service/mixture/MixtureService";
 import {toMixtureInfo} from "@/puff-smith/service/mixture/utils";
 import prisma from "@/puff-smith/service/side-effect/prisma";
@@ -15,16 +16,28 @@ export default function MixtureJob(agenda: Agenda) {
 		await jobProgress.total((maxNicotine + 1) * await prisma.aroma.count() * await prisma.booster.count() * await prisma.base.count());
 		const mixtureService = MixtureService();
 		await prisma.mixture.deleteMany();
+
+		const toCreate = (mixture: IMixtureCreate) => {
+			const vgToRound = Math.round(mixture.vg * 0.1) / 0.1;
+			return {
+				...mixture,
+				vgToRound,
+				pgToRound: 100 - vgToRound,
+				nicotineToRound: Math.round(mixture.nicotine || 0),
+			};
+		};
+
 		/**
-		 * For loop through nicotine strengths (0-20mg/ml).
+		 * run through all aromas...
 		 */
-		for (let nicotine = 0; nicotine <= maxNicotine; nicotine++) {
-			/**
-			 * run through all aromas...
-			 */
-			for (const aroma of await prisma.aroma.findMany()) {
-				for (const booster of await prisma.booster.findMany()) {
-					for (const base of await prisma.base.findMany()) {
+		for (const aroma of await prisma.aroma.findMany()) {
+			for (const booster of await prisma.booster.findMany()) {
+				for (const base of await prisma.base.findMany()) {
+					const batch: ReturnType<typeof toCreate>[] = [];
+					/**
+					 * For loop through nicotine strengths
+					 */
+					for (let nicotine = 0; nicotine <= maxNicotine; nicotine++) {
 						await progress(async () => {
 							const info = await toMixtureInfo({
 								nicotine,
@@ -33,7 +46,7 @@ export default function MixtureJob(agenda: Agenda) {
 								base,
 							});
 							const volume = aroma.volume?.toNumber() || aroma.content.toNumber();
-							return mixtureService.create({
+							batch.push(toCreate({
 								aromaId: aroma.id,
 								baseId: info.base?.baseId,
 								baseMl: info.base?.volume || 0,
@@ -49,9 +62,10 @@ export default function MixtureJob(agenda: Agenda) {
 								pgToMl: info.result.ml.pg,
 								nicotine: info.result.nicotine,
 								error: info.result.error,
-							});
+							}));
 						});
 					}
+					prisma.mixture.createMany({data: batch});
 				}
 			}
 		}
