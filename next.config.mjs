9aa71a59ad94f71fa887009628095ec8cd38e15d
Marek Hanzal <marek.hanzal@x32.cz>
2022-05-02 17:32:54 +0200

    Common: Reverted to React 17

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ import withPlugins from 'next-compose-plugins';
 import { patchWebpackConfig } from 'next-global-css';
 import images from 'next-images';
 import withTM from 'next-transpile-modules';
+import fs from 'node:fs';
 import { merge } from 'webpack-merge';
 
 export default withPlugins([
@@ -29,11 +30,9 @@ export default withPlugins([
 			return merge(config, {
 				entry() {
 					return config.entry().then(entry => {
-						entry = {
-							...entry,
-							// agenda:    ['./src/cli/agenda.ts'],
-							// migration: ['./src/cli/migration.ts'],
-						};
+						fs.readdirSync('./src/cli').filter(file => file.endsWith('.ts') && fs.statSync(`./src/cli/${file}`).isFile()).forEach(file => {
+							entry[file.replace('.ts', '')] = [`./src/cli/${file}`];
+						});
 						Object.keys(entry).map(key => {
 							entry[key] = {import: ['./src/service/side-effect/bootstrap.ts', ...entry[key]]};
 						});
