22b8af4e1ecb689b9de635d7480f264ebd46a67e
Marek Hanzal <marek.hanzal@x32.cz>
2022-05-06 20:07:06 +0200

    Common: A little improvement

--- src/site/shared/mixture/@module/view/MixturePreview.tsx
+++ src/site/shared/mixture/@module/view/MixturePreview.tsx
@@ -1,7 +1,8 @@
 import {NicotineInline} from "@/puff-smith/component/inline/NicotineInline";
 import {PgVgInline} from "@/puff-smith/component/inline/PgVgInline";
 import {IMixture} from "@/puff-smith/service/mixture/interface";
-import {MixtureAromaInline} from "@/puff-smith/site/shared/mixture/@module/inline/MixtureAromaInline";
+import {AromaContentInline} from "@/puff-smith/site/shared/aroma/@module/inline/AromaContentInline";
+import {AromaNameInline} from "@/puff-smith/site/shared/aroma/@module/inline/AromaNameInline";
 import {MixtureBaseInline} from "@/puff-smith/site/shared/mixture/@module/inline/MixtureBaseInline";
 import {MixtureBoosterInline} from "@/puff-smith/site/shared/mixture/@module/inline/MixtureBoosterInline";
 import {Preview} from "@leight-core/client";
@@ -15,19 +16,20 @@ export interface IMixturePreviewProps {
 export const MixturePreview: FC<IMixturePreviewProps> = ({mixture}) => {
 	return <Row>
 		<Col span={10}>
-			<Preview translation={"lab.mixture.preview"} size={"small"} hideEmpty>
+			<Preview translation={"lab.mixture.preview"} size={"small"}>
 				{{
-					pgvg: <PgVgInline pgvg={mixture}/>,
-					nicotine: <NicotineInline nicotine={mixture.nicotine}/>,
-					aroma: <MixtureAromaInline mixture={mixture}/>,
+					aroma: <AromaNameInline aroma={mixture.aroma}/>,
+					base: mixture.base && <MixtureBaseInline mixture={mixture}/>,
+					booster: mixture.booster && <MixtureBoosterInline mixture={mixture}/>,
 				}}
 			</Preview>
 		</Col>
 		<Col span={14}>
 			<Preview translation={"lab.mixture.preview"} size={"small"}>
 				{{
-					booster: mixture.booster && <MixtureBoosterInline mixture={mixture}/>,
-					base: mixture.base && <MixtureBaseInline mixture={mixture}/>,
+					aromaContent: <AromaContentInline aroma={mixture.aroma}/>,
+					pgvg: <PgVgInline pgvg={mixture}/>,
+					nicotine: <NicotineInline nicotine={mixture.nicotine}/>,
 				}}
 			</Preview>
 		</Col>
