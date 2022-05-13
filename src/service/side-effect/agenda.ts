61c66e5088cbd7ee99762b83bfb785efcadae59c
Marek Hanzal <marek.hanzal@x32.cz>
2022-05-13 12:03:57 +0200

    Common: ...

--- src/service/side-effect/agenda.ts
+++ src/service/side-effect/agenda.ts
@@ -3,7 +3,7 @@ import {Agenda as CoolAgenda} from "agenda";
 const agenda = new CoolAgenda({
 	defaultConcurrency: 10,
 	defaultLockLimit: 10,
-	defaultLockLifetime: 60 * 1000 /** a minute */ * 60 /** an hour */,
+	defaultLockLifetime: 60 * 1000 /** a minute */ * 60 /** an hour */ * 3 /** this is quite a time... */,
 	name: "Puff Smith",
 	maxConcurrency: 20,
 	db: process.env.AGENDA_DB ? {
