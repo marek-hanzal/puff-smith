a7f001272d38c92b7d07926c999de6394282b83a
Marek Hanzal <marek.hanzal@x32.cz>
2022-05-27 17:12:44 +0200

    Common: Another fixes VII

--- src/site/lab/liquid/@module/list/LiquidListToolbar.tsx
+++ src/site/lab/liquid/@module/list/LiquidListToolbar.tsx
@@ -1,6 +1,6 @@
 import {DeleteConfirmButton} from "@/puff-smith/component/button/DeleteConfirmButton";
 import {useDeleteMutation} from "@/sdk/api/liquid/delete";
-import {useLiquidQueryInvalidate} from "@/sdk/api/liquid/query";
+import {useLiquidCountQueryInvalidate, useLiquidQueryInvalidate} from "@/sdk/api/liquid/query";
 import {ButtonBar, IButtonBarProps} from "@leight-core/client";
 import {FC} from "react";
 
@@ -8,11 +8,16 @@ interface ILiquidListToolbarProps extends Partial<IButtonBarProps> {
 }
 
 export const LiquidListToolbar: FC<ILiquidListToolbarProps> = props => {
+	const liquidQueryInvalidate = useLiquidQueryInvalidate();
+	const liquidCountQueryInvalidate = useLiquidCountQueryInvalidate();
 	return <ButtonBar size={4} {...props}>
 		<DeleteConfirmButton
 			translation={"lab.liquid"}
 			mutator={useDeleteMutation()}
-			invalidator={useLiquidQueryInvalidate()}
+			invalidator={async () => {
+				await liquidQueryInvalidate();
+				await liquidCountQueryInvalidate();
+			}}
 		/>
 	</ButtonBar>;
 };
