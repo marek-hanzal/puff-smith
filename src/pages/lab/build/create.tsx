b6ade6b3e6de106d2d9d69751965d94058e78b5c
Marek Hanzal <marek.hanzal@x32.cz>
2022-06-01 19:30:48 +0200

    Common: ...

--- src/pages/lab/build/create.tsx
+++ src/pages/lab/build/create.tsx
@@ -4,7 +4,7 @@ import {withLabLayout} from "@/puff-smith/site/lab/@module/layout/layout";
 import {BuildCreateForm} from "@/puff-smith/site/lab/build/@module/form/BuildCreateForm";
 import {ButtonLink, ListIcon, Template} from "@leight-core/client";
 
-export default withLabLayout(function Index() {
+export default withLabLayout(function Create() {
 	return <LabPage
 		title={"lab.build.create"}
 		menuSelection={["/lab/build"]}
