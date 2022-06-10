516d6883b1dda89adef23a408a85f138c7b518ca
Marek Hanzal <marek.hanzal@x32.cz>
2022-06-10 15:13:59 +0200

    Common: Improved migration a bit

--- prisma/migrations/20220607163747_build_drain_watts_migration/migration.ts
+++ prisma/migrations/20220607163747_build_drain_watts_migration/migration.ts
@@ -1,19 +1,14 @@
 import {IMigration} from "@/puff-smith/cli/migration";
 import {OhmService} from "@/puff-smith/service/ohm/OhmService";
 import "@/puff-smith/service/side-effect/bootstrap";
+import {Prisma} from "@prisma/client";
 
 export const up: IMigration = async ({context: prisma}) => {
-	const builds = await prisma.build.findMany();
+	const builds = await prisma.$queryRaw<{ id: string, ohm: Prisma.Decimal }[]>`SELECT id, ohm FROM Build`;
 	const ohmService = OhmService();
-	for (const build of builds) {
-		const drain = ohmService.toAmps(3.7, build.ohm.toNumber());
+	for (const {id, ohm} of builds) {
+		const drain = ohmService.toAmps(3.7, ohm.toNumber());
 		const watts = ohmService.toWatt(3.7, drain);
-		await prisma.build.update({
-			where: {id: build.id},
-			data: {
-				drain,
-				watts,
-			}
-		});
+		await prisma.$executeRaw`UPDATE Build SET drain=${drain}, watts=${watts} WHERE id = ${id}`;
 	}
 };
