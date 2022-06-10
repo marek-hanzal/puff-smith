1e99d91290a3ea7a02d664c2b70451a5a755ef5b
Marek Hanzal <marek.hanzal@x32.cz>
2022-06-10 14:33:41 +0200

    Common: ...

--- src/cli/migration.ts
+++ src/cli/migration.ts
@@ -1,15 +1,31 @@
 import "@/puff-smith/service/side-effect/bootstrap";
 import prisma from "@/puff-smith/service/side-effect/prisma";
-import {runSql} from "@leight-core/server";
+import {sha256} from "@/puff-smith/service/utils/sha256";
+import {executeSql, runSql} from "@leight-core/server";
+import fs from "node:fs";
 import path from "node:path";
-import {JSONStorage, Umzug} from "umzug";
+import {Umzug} from "umzug";
+import {v4} from "uuid";
+
+const EMPTY_MIGRATION = "__empty__";
 
 const umzug = new Umzug({
 	migrations: {
 		glob: process.cwd() + "/prisma/migrations/**/*.{ts,sql}",
 		resolve: params => {
 			const {context: prisma} = params;
-			const name = path.normalize(params.path!!.replace(process.cwd(), "")).replaceAll("\\", "/");
+			const name = path.normalize(params.path!.replace(process.cwd(), ""))
+				.replaceAll("\\", "/")
+				.replaceAll("/prisma/migrations", "")
+				.replaceAll(/migration.(ts|sql)/g, "")
+				.replaceAll("/", "");
+			if (fs.readFileSync(params.path!).length === 0) {
+				return {
+					name: EMPTY_MIGRATION,
+					up: async () => {
+					},
+				};
+			}
 			if (params.path?.endsWith(".sql")) {
 				return {
 					name,
@@ -23,11 +39,38 @@ const umzug = new Umzug({
 			};
 		},
 	},
-	storage: new JSONStorage({
-		path: "./.data/umzug.json",
-	}),
+	storage: {
+		logMigration: async ({context: prisma, ...params}) => {
+			if (params.name === EMPTY_MIGRATION) {
+				return;
+			}
+			try {
+				await executeSql(`
+					create table _prisma_migrations
+					(
+						id                  varchar(36)  not null primary key,
+						checksum            varchar(64)  not null,
+						finished_at         timestamp with time zone,
+						migration_name      varchar(255) not null,
+						logs                text,
+						rolled_back_at      timestamp with time zone,
+						started_at          timestamp with time zone default now() not null,
+						applied_steps_count integer                  default 0 not null
+					);
+
+					alter table _prisma_migrations owner to "puff-smith";
+				`, prisma);
+			} catch (e) {
+			}
+			const hash = sha256(fs.readFileSync(params.path!).toString());
+			await prisma.$executeRaw`INSERT INTO _prisma_migrations VALUES (${v4()}, ${hash}, NOW(), ${params.name}, NULL, NULL, now(), 1)`;
+		},
+		unlogMigration: async params => {
+		},
+		executed: async ({context: prisma}) => (await prisma.$queryRaw<{ migration_name: string }[]>`SELECT migration_name FROM _prisma_migrations ORDER BY migration_name ASC`).map(({migration_name}) => migration_name),
+	},
 	context: prisma,
-	logger: console,
+	logger: undefined,
 });
 
 export type IMigration = typeof umzug._types.migration;
