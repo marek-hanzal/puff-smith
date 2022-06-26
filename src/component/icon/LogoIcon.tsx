8f329897e60a00262a460757486bfabac81d374d
Marek Hanzal <marek.hanzal@x32.cz>
2022-06-26 02:42:15 +0200

    Common: Version bump

--- src/component/icon/LogoIcon.tsx
+++ src/component/icon/LogoIcon.tsx
@@ -3,4 +3,4 @@ import {Image} from "antd";
 import {ImageProps} from "rc-image";
 import {FC} from "react";
 
-export const LogoIcon: FC<ImageProps> = props => <Image alt={"Logo"} preview={false} src={Logo} style={{width: "10vw"}} {...props}/>;
+export const LogoIcon: FC<ImageProps> = props => <Image alt={"Logo"} preview={false} src={Logo} {...props}/>;
