0cbae550974512cd79cabd46cff37a8a38c0789f
Marek Hanzal <marek.hanzal@x32.cz>
2022-09-15 18:31:19 +0200

    Common: Experiment with floating plus

--- src/pages/market/aroma/index.tsx
+++ src/pages/market/aroma/index.tsx
@@ -4,6 +4,8 @@ import {MobileMarketPage} from "@/puff-smith/site/market/@module/component/Mobil
 import {withMarketLayout} from "@/puff-smith/site/market/@module/layout/layout";
 import {AromaList} from "@/puff-smith/ui/aroma/list/AromaList";
 import {AromaProviderControl} from "@/sdk/api/aroma/query";
+import {FloatingBubble} from "antd-mobile";
+import {AddOutline} from "antd-mobile-icons";
 
 export default withMarketLayout(function Index() {
 	return <>
@@ -11,6 +13,17 @@ export default withMarketLayout(function Index() {
 			title={"market.aroma.index"}
 			icon={<AromaIcon/>}
 		>
+			<FloatingBubble
+				axis="x"
+				magnetic="x"
+				style={{
+					"--initial-position-bottom": "24px",
+					"--initial-position-right": "24px",
+					"--edge-distance": "24px",
+				}}
+			>
+				<AddOutline fontSize={32}/>
+			</FloatingBubble>
 			<AromaProviderControl
 				defaultSize={DEFAULT_LIST_SIZE}
 			>
