323dd67abee9d1992eb55e3561884fffbb590f13
Marek Hanzal <marek.hanzal@x32.cz>
2022-09-15 18:50:17 +0200

    Common: Another experiment with toast

--- src/pages/market/aroma/index.tsx
+++ src/pages/market/aroma/index.tsx
@@ -4,15 +4,41 @@ import {MobileMarketPage} from "@/puff-smith/site/market/@module/component/Mobil
 import {withMarketLayout} from "@/puff-smith/site/market/@module/layout/layout";
 import {AromaList} from "@/puff-smith/ui/aroma/list/AromaList";
 import {AromaProviderControl} from "@/sdk/api/aroma/query";
-import {FloatingBubble} from "antd-mobile";
+import {ActionSheet, Dialog, FloatingBubble, Toast} from "antd-mobile";
 import {AddOutline} from "antd-mobile-icons";
+import {useState} from "react";
 
 export default withMarketLayout(function Index() {
+	const [visible, setVisible] = useState(false);
 	return <>
 		<MobileMarketPage
 			title={"market.aroma.index"}
 			icon={<AromaIcon/>}
 		>
+			<ActionSheet
+				visible={visible}
+				actions={[
+					{text: "foo", key: "aa"},
+					{text: "bar", key: "bb"},
+					{
+						text: "bleh", key: "cc", onClick: async () => {
+							if (await Dialog.confirm({
+								content: "Boo？",
+								confirmText: "yep",
+								cancelText: "nope",
+								onConfirm: () => setVisible(false),
+							})) {
+								Toast.show({
+									icon: "loading",
+									duration: 5000,
+									maskClickable: false,
+								});
+							}
+						}
+					},
+				]}
+				onClose={() => setVisible(false)}
+			/>
 			<FloatingBubble
 				axis="x"
 				magnetic="x"
@@ -21,6 +47,7 @@ export default withMarketLayout(function Index() {
 					"--initial-position-right": "24px",
 					"--edge-distance": "24px",
 				}}
+				onClick={() => setVisible(true)}
 			>
 				<AddOutline fontSize={32}/>
 			</FloatingBubble>
