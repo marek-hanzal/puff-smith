774c9950d6ef48dbd0158105c559ffe3403b9b4f
Marek Hanzal <marek.hanzal@x32.cz>
2022-09-16 12:18:35 +0200

    Common: Experiment with button position

--- src/pages/market/aroma/index.tsx
+++ src/pages/market/aroma/index.tsx
@@ -4,24 +4,24 @@ import {MobileMarketPage} from "@/puff-smith/site/market/@module/component/Mobil
 import {withMarketLayout} from "@/puff-smith/site/market/@module/layout/layout";
 import {AromaList} from "@/puff-smith/ui/aroma/list/AromaList";
 import {AromaProviderControl} from "@/sdk/api/aroma/query";
-import {ActionSheet, Dialog, FloatingBubble, Toast} from "antd-mobile";
-import {AddOutline} from "antd-mobile-icons";
-import {useState} from "react";
+import {BubbleMenu} from "@leight-core/client";
+import {Dialog, Toast} from "antd-mobile";
 
 export default withMarketLayout(function Index() {
-	const [visible, setVisible] = useState(false);
 	return <>
 		<MobileMarketPage
 			title={"market.aroma.index"}
 			icon={<AromaIcon/>}
 		>
-			<ActionSheet
-				visible={visible}
+			<BubbleMenu
 				actions={[
+					{
+						key: "aaa",
+					},
 					{text: "foo", key: "aa"},
 					{text: "bar", key: "bb"},
 					{
-						text: "bleh", key: "cc", onClick: async () => {
+						text: "bleh", key: "cc", onClick: async ({setVisible}) => {
 							if (await Dialog.confirm({
 								content: "Boo？",
 								confirmText: "yep",
@@ -37,20 +37,8 @@ export default withMarketLayout(function Index() {
 						}
 					},
 				]}
-				onClose={() => setVisible(false)}
 			/>
-			<FloatingBubble
-				axis="x"
-				magnetic="x"
-				style={{
-					"--initial-position-bottom": "24px",
-					"--initial-position-right": "24px",
-					"--edge-distance": "24px",
-				}}
-				onClick={() => setVisible(true)}
-			>
-				<AddOutline fontSize={32}/>
-			</FloatingBubble>
+
 			<AromaProviderControl
 				defaultSize={DEFAULT_LIST_SIZE}
 			>
