9a1870defd14f7eba745f2bd52cea0cd4d57e666
Marek Hanzal <marek.hanzal@x32.cz>
2022-09-30 18:37:42 +0200

    Common: Fixes

--- src/pages/market/aroma/[aromaId]/index.tsx
+++ src/pages/market/aroma/[aromaId]/index.tsx
@@ -3,6 +3,7 @@ import {AromaSource} from "@/puff-smith/service/aroma/AromaSource";
 import {IAromaFetch} from "@/puff-smith/service/aroma/interface";
 import {MobileMarketPage} from "@/puff-smith/site/market/@module/component/MobileMarketPage";
 import {withMarketLayout} from "@/puff-smith/site/market/@module/layout/layout";
+import {AromaAromaBubble} from "@/puff-smith/ui/aroma/menu/AromaAromaBubble";
 import {AromaView} from "@/puff-smith/ui/aroma/view/AromaView";
 
 export default withMarketLayout(function Index({aroma}: IAromaFetch) {
@@ -13,6 +14,7 @@ export default withMarketLayout(function Index({aroma}: IAromaFetch) {
 		menuSelection={["/market/aroma", "/market/aroma/[aromaId]"]}
 		icon={<AromaIcon/>}
 	>
+		<AromaAromaBubble aroma={aroma}/>
 		<AromaView aroma={aroma}/>
 	</MobileMarketPage>;
 });
