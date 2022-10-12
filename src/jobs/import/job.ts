bee14e22559e84ece16987d38083c9c88fcd9b4f
Marek Hanzal <marek.hanzal@x32.cz>
2022-10-12 20:20:43 +0200

    Common: Improved and some things fixed

--- src/jobs/import/job.ts
+++ src/jobs/import/job.ts
@@ -1,32 +1,22 @@
 import {
 	IImportJobParams,
 	IMPORT_JOB
-}                          from "@/puff-smith/jobs/import/interface";
-import {AromaSource}       from "@/puff-smith/service/aroma/AromaSource";
-import {Container}         from "@/puff-smith/service/Container";
-import {JobSource}         from "@/puff-smith/service/job/JobSource";
-import fileService         from "@/puff-smith/service/side-effect/fileService";
-import {TagSource}         from "@/puff-smith/service/tag/TagSource";
-import {TokenSource}       from "@/puff-smith/service/token/TokenSource";
-import {TranslationSource} from "@/puff-smith/service/translation/TranslationSource";
-import {UserSource}        from "@/puff-smith/service/user/UserSource";
-import {VendorSource}      from "@/puff-smith/service/vendor/VendorSource";
-import {IJobProcessor}     from "@leight-core/api";
-import {toImport}          from "@leight-core/server";
-import PQueue              from "p-queue";
-import xlsx                from "xlsx";
+}                      from "@/puff-smith/jobs/import/interface";
+import {Container}     from "@/puff-smith/service/Container";
+import {JobSource}     from "@/puff-smith/service/job/JobSource";
+import fileService     from "@/puff-smith/service/side-effect/fileService";
+import {UserSource}    from "@/puff-smith/service/user/UserSource";
+import {IJobProcessor} from "@leight-core/api";
+import {toImport}      from "@leight-core/server";
+import PQueue          from "p-queue";
+import xlsx            from "xlsx";
 
-const sources = [
-	AromaSource(),
-	TagSource(),
-	TokenSource(),
-	TranslationSource(),
-	VendorSource(),
-];
-
-export const ImportJob: IJobProcessor<IImportJobParams> = JobSource().processor(IMPORT_JOB, async ({logger, job, params: {fileId}, jobProgress}) => {
-	const labels = {jobId: job.id};
-	logger       = logger.child({labels, jobId: labels.jobId});
+export const ImportJob: IJobProcessor<IImportJobParams> = JobSource().processor(IMPORT_JOB, async ({logger, job, params: {fileId}, jobProgress, userId}) => {
+	const container = Container({
+		user: await UserSource().asUser(userId),
+	});
+	const labels    = {jobId: job.id};
+	logger          = logger.child({labels, jobId: labels.jobId});
 	logger.info("Checking fileId");
 	if (!fileId) {
 		await jobProgress.setResult("REVIEW");
@@ -41,7 +31,18 @@ export const ImportJob: IJobProcessor<IImportJobParams> = JobSource().processor(
 		job,
 		jobProgress,
 		workbook,
-		sources,
+		sources:   [
+			await container.useAromaSource(async source => source),
+			await container.useBaseSource(async source => source),
+			await container.useBoosterSource(async source => source),
+			await container.useLiquidSource(async source => source),
+			await container.useRecipeSource(async source => source),
+			await container.useTagSource(async source => source),
+			await container.useTokenSource(async source => source),
+			await container.useTranslationSource(async source => source),
+			await container.useUserSource(async source => source),
+			await container.useVendorSource(async source => source),
+		],
 	});
 }, options => new PQueue({
 	...options,
