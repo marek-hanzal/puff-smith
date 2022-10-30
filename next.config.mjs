ade257f324b5ee0d6d715ca1a3896f76daaf98aa
Marek Hanzal <marek.hanzal@x32.cz>
2022-10-30 23:36:58 +0100

    Updated next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,9 @@
 // @ts-check
-import { i18n } from './next-i18next.config';
-
 !process.env.SKIP_ENV_VALIDATION && (await import('./src/env/server.mjs'));
 
 /** @type {import('next').NextConfig} */
 const config = {
     reactStrictMode: true,
     swcMinify:       true,
-    i18n,
 };
 export default config;
