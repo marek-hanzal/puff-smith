248d834f7ec3ee6ab9fc18829b7b67ac44e65394
Marek Hanzal <marek.hanzal@x32.cz>
2022-12-22 16:21:04 +0100

    A little things

--- apps/puff-smith/src/pages/_app.tsx
+++ apps/puff-smith/src/pages/_app.tsx
@@ -45,7 +45,7 @@ const PuffSmith = (props: AppProps & { colorScheme: ColorScheme }) => {
         <Head>
             <title>Puff Smith</title>
             <meta name={"viewport"} content={"minimum-scale=1, initial-scale=1, width=device-width"}/>
-            {/*<link rel="shortcut icon" href="/favicon.svg" />*/}
+            <link rel="shortcut icon" href="/favicon.ico"/>
         </Head>
         <ColorSchemeProvider
             colorScheme={colorScheme}
